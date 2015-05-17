63cf98e769d0b0e1e01e2dc6fd7e35c2ae8cd1f8
etiennestuder <etienne@studer.nu>
2015-05-17 07:06:10 +0200

    Improve icon names and avoid reusing them

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/PluginImages.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/PluginImages.java
@@ -44,9 +44,10 @@ public enum PluginImages implements PluginImage {
     REMOVE_CONSOLE(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/remove_page.png", ImageState.DISABLED, "icons/full/dlcl16/remove_page.png")),
     REMOVE_ALL_CONSOLES(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/remove_all_pages.png", ImageState.DISABLED, "icons/full/dlcl16/remove_all_pages.png")),
     CANCEL_TASK_EXECUTION(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/cancel_build_execution.png", ImageState.DISABLED, "icons/full/dlcl16/cancel_build_execution.png")),
-    RUN_CONFIG_GRADLE_DISTRIBUTION(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/gradle_distribution.png")),
-    RUN_CONFIG_ARGUMENTS(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/arguments.gif")),
-    GRADLE_ICON(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/gradle_distribution.png")),
+    RUN_CONFIG_TASKS(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/run_config_tasks.png")),
+    RUN_CONFIG_GRADLE_DISTRIBUTION(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/run_config_gradle_distribution.png")),
+    RUN_CONFIG_ARGUMENTS(ImmutableMap.of(ImageState.ENABLED, "icons/full/eview16/run_config_arguments.gif")),
+    OPERATION_ROOT(ImmutableMap.of(ImageState.ENABLED, "icons/full/obj16/operation_root.png")),
     OPERATION_SUCCESS(ImmutableMap.of(ImageState.ENABLED, "icons/full/obj16/operation_success.png")),
     OPERATION_FAILURE(ImmutableMap.of(ImageState.ENABLED, "icons/full/obj16/operation_failure.png"));
     // @formatter:on
