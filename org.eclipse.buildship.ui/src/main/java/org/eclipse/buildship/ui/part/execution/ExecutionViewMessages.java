ec43c8a037b6863d74522b10533833b1a8c1fa51
Donat Csikos <csdonat@gmail.com>
2015-05-18 16:24:41 +0200

    Comply with Checkstyle
    Signed-off-by: Donat Csikos <csdonat@gmail.com>

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/ExecutionViewMessages.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/ExecutionViewMessages.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2015 the original author or authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Etienne Studer & Donát Csikós (Gradle Inc.) - initial API and implementation and initial documentation
+ */
+
 package org.eclipse.buildship.ui.part.execution;
 
 import org.eclipse.osgi.util.NLS;
 
+/**
+ * Lists the i18n resource keys for the execution view messages.
+ */
 public class ExecutionViewMessages extends NLS {
 
     private static final String BUNDLE_NAME = "org.eclipse.buildship.ui.part.execution.ExecutionViewMessages"; //$NON-NLS-1$
