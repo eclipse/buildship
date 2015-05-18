b1b78e8d7ef17cefdb33cd7a280ee4eb73f2b2e4
Donat Csikos <csdonat@gmail.com>
2015-05-18 16:05:03 +0200

    Minor
    Signed-off-by: Donat Csikos <csdonat@gmail.com>

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/ExecutionViewMessages.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/ExecutionViewMessages.java
@@ -5,13 +5,17 @@ import org.eclipse.osgi.util.NLS;
 public class ExecutionViewMessages extends NLS {
 
     private static final String BUNDLE_NAME = "org.eclipse.buildship.ui.part.execution.ExecutionViewMessages"; //$NON-NLS-1$
+
     public static String Label_No_Execution;
+
     public static String Tree_Column_Duration_Text;
     public static String Tree_Column_Operation_Text;
+
     public static String Tree_Item_Root_Text;
+    public static String Tree_Item_Tests_Text;
     public static String Tree_Item_Test_Finished_Text;
     public static String Tree_Item_Test_Started_Text;
-    public static String Tree_Item_Tests_Text;
+
     static {
         // initialize resource bundle
         NLS.initializeMessages(BUNDLE_NAME, ExecutionViewMessages.class);
