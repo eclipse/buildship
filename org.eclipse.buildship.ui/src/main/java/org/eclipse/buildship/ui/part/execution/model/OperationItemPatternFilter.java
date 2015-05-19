ad7ca08ea18b039472a65948b08eca3454aa8dc0
etiennestuder <etienne@studer.nu>
2015-05-19 12:15:44 +0200

    Fix checkstyle

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/model/OperationItemPatternFilter.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/part/execution/model/OperationItemPatternFilter.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2015 the original author or authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Simon Scholz (vogella GmbH) - initial API and implementation and initial documentation
+ */
+
 package org.eclipse.buildship.ui.part.execution.model;
 
 import org.eclipse.jface.viewers.Viewer;
@@ -5,16 +16,19 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.buildship.ui.viewer.PatternFilter;
 
 
-public class OperationItemPatternFilter extends PatternFilter {
+/**
+ * Filters out {@code OperationItem} instances whose label does not match the given text.
+ */
+public final class OperationItemPatternFilter extends PatternFilter {
 
     @Override
     protected boolean isLeafMatch(Viewer viewer, Object element) {
         if (element instanceof OperationItem) {
             String label = ((OperationItem) element).getLabel();
-
             return wordMatches(label);
         }
 
         return super.isLeafMatch(viewer, element);
     }
+
 }
