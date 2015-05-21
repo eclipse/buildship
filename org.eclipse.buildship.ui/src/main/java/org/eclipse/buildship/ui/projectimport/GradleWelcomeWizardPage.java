4c2f05cefac671d3ee45330b4087b394c47398a6
Donat Csikos <csdonat@gmail.com>
2015-05-21 13:34:55 +0200

    Fix checkstyle violataion
    Signed-off-by: Donat Csikos <csdonat@gmail.com>

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/projectimport/GradleWelcomeWizardPage.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/projectimport/GradleWelcomeWizardPage.java
@@ -26,7 +26,6 @@ import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
 /**
