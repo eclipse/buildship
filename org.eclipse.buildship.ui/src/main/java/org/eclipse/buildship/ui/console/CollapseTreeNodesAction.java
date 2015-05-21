15cb619dd4e2af73dcccf195a874b7ebf5319ed0
etiennestuder <etienne@studer.nu>
2015-05-21 16:14:37 +0200

    Add actions to expand/collapse nodes

---
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/console/CollapseTreeNodesAction.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 the original author or authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Etienne Studer & Donát Csikós (Gradle Inc.) - initial API and implementation and initial documentation
+ */
+
+package org.eclipse.buildship.ui.console;
+
+import com.google.common.base.Preconditions;
+import org.eclipse.buildship.ui.PluginImage.ImageState;
+import org.eclipse.buildship.ui.PluginImages;
+import org.eclipse.buildship.ui.generic.NodeSelection;
+import org.eclipse.buildship.ui.generic.SelectionSpecificAction;
+import org.eclipse.buildship.ui.i18n.UiMessages;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Event;
+
+/**
+ * Collapses all the nodes under the selected node or the entire tree if no node is selected.
+ */
+public final class CollapseTreeNodesAction extends Action implements SelectionSpecificAction {
+
+    private final AbstractTreeViewer treeViewer;
+
+    public CollapseTreeNodesAction(AbstractTreeViewer treeViewer) {
+        this.treeViewer = Preconditions.checkNotNull(treeViewer);
+
+        setToolTipText(UiMessages.Action_CollapseNode_Tooltip);
+        setImageDescriptor(PluginImages.COLLAPSE_NODE.withState(ImageState.ENABLED).getImageDescriptor());
+    }
+
+    @Override
+    public void runWithEvent(Event event) {
+        ITreeSelection selection = this.treeViewer.getStructuredSelection();
+        if (selection.isEmpty()) {
+            this.treeViewer.collapseAll();
+        } else {
+            for (Object element : selection.toList()) {
+                this.treeViewer.collapseToLevel(element, TreeViewer.ALL_LEVELS);
+            }
+        }
+    }
+
+    @Override
+    public boolean isVisibleFor(NodeSelection selection) {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabledFor(NodeSelection selection) {
+        return true;
+    }
+
+    @Override
+    public void setEnabledFor(NodeSelection selection) {
+        setEnabled(true);
+    }
+
+}
