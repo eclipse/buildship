dc9e22003ae27ee26af25864192f6f7a64747a2a
Simon Scholz <simon.scholz@vogella.com>
2015-05-22 21:29:32 +0200

    OperationItems for tests do now only show the name instead of the whole classname
    Change-Id: I5adf351b61a6a451b5ca8253c4c5af8c60653208
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/view/execution/OperationItemConfigurator.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/view/execution/OperationItemConfigurator.java
@@ -11,11 +11,21 @@
 
 package org.eclipse.buildship.ui.view.execution;
 
+import org.gradle.tooling.events.FailureResult;
+import org.gradle.tooling.events.FinishEvent;
+import org.gradle.tooling.events.OperationDescriptor;
+import org.gradle.tooling.events.OperationResult;
+import org.gradle.tooling.events.ProgressEvent;
+import org.gradle.tooling.events.SkippedResult;
+import org.gradle.tooling.events.StartEvent;
+import org.gradle.tooling.events.SuccessResult;
+import org.gradle.tooling.events.test.TestOperationDescriptor;
+
+import org.eclipse.osgi.util.NLS;
+
 import org.eclipse.buildship.core.GradlePluginsRuntimeException;
 import org.eclipse.buildship.ui.PluginImage;
 import org.eclipse.buildship.ui.PluginImages;
-import org.eclipse.osgi.util.NLS;
-import org.gradle.tooling.events.*;
 
 /**
  * Configures an {@code OperationItem} instance from an event belonging associated with that item.
@@ -23,6 +33,9 @@ import org.gradle.tooling.events.*;
 public final class OperationItemConfigurator {
 
     public void configure(OperationItem operationItem, ProgressEvent event) {
+
+        handleOperationItemName(operationItem, event);
+
         if (event instanceof StartEvent) {
             operationItem.setDuration(ExecutionsViewMessages.Tree_Item_Operation_Started_Text);
         } else if (event instanceof FinishEvent) {
@@ -37,6 +50,13 @@ public final class OperationItemConfigurator {
             }
         } else {
             throw new GradlePluginsRuntimeException("Unsupported event type: " + event.getClass());
+        }
+    }
+
+    private void handleOperationItemName(OperationItem operationItem, ProgressEvent event) {
+        OperationDescriptor descriptor = event.getDescriptor();
+        if (descriptor instanceof TestOperationDescriptor) {
+            operationItem.setName(descriptor.getName());
         }
     }
 
