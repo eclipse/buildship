f9bbd611564e04e8a36dfe807f292025d668184d
etiennestuder <etienne@studer.nu>
2015-05-27 22:46:19 +0200

    Add API openInInternalEditor(IFile file, boolean activate)

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/util/editor/EditorUtils.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/util/editor/EditorUtils.java
@@ -12,6 +12,7 @@
 package org.eclipse.buildship.ui.util.editor;
 
 import org.eclipse.buildship.ui.UiPlugin;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -28,14 +29,36 @@ public final class EditorUtils {
     private EditorUtils() {
     }
 
-    public static void openInInternalEditor(File file, boolean activate) {
-        String editorId;
-        IEditorDescriptor desc = getEditorDescriptor(file);
-        if (desc == null || !desc.isInternal()) {
-            editorId = "org.eclipse.ui.DefaultTextEditor"; //$NON-NLS-1$
-        } else {
-            editorId = desc.getId();
+    public static void openInInternalEditor(IFile file, boolean activate) {
+        IEditorDescriptor desc;
+        try {
+            desc = IDE.getEditorDescriptor(file);
+        } catch (PartInitException e1) {
+            // thrown if no editor can be found
+            desc = null;
         }
+
+        String editorId = getInternalEditorId(desc);
+
+        try {
+            IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+            IDE.openEditor(activePage, file, editorId, activate);
+        } catch (PartInitException e) {
+            String message = String.format("Cannot open file %s in editor.", file.getFullPath());
+            UiPlugin.logger().error(message, e); //$NON-NLS-1$
+        }
+    }
+
+    public static void openInInternalEditor(File file, boolean activate) {
+        IEditorDescriptor desc;
+        try {
+            desc = IDE.getEditorDescriptor(file.getName());
+        } catch (PartInitException e1) {
+            // thrown if no editor can be found
+            desc = null;
+        }
+
+        String editorId = getInternalEditorId(desc);
 
         try {
             IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
@@ -46,13 +69,14 @@ public final class EditorUtils {
         }
     }
 
-    private static IEditorDescriptor getEditorDescriptor(File buildScript) {
-        try {
-            return IDE.getEditorDescriptor(buildScript.getName());
-        } catch (PartInitException e) {
-            // thrown if no editor can be found
-            return null;
+    private static String getInternalEditorId(IEditorDescriptor desc) {
+        String editorId;
+        if (desc == null || !desc.isInternal()) {
+            editorId = "org.eclipse.ui.DefaultTextEditor"; //$NON-NLS-1$
+        } else {
+            editorId = desc.getId();
         }
+        return editorId;
     }
 
 }
