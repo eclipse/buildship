292b6612136c208a192c9c8b4db0338dc3122d65
etiennestuder <etienne@studer.nu>
2015-05-27 23:01:20 +0200

    Add API selectAndReveal

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/util/editor/EditorUtils.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/util/editor/EditorUtils.java
@@ -11,15 +11,20 @@
 
 package org.eclipse.buildship.ui.util.editor;
 
+import org.eclipse.buildship.core.GradlePluginsRuntimeException;
 import org.eclipse.buildship.ui.UiPlugin;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.*;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Contains helper methods related to interacting with the Eclipse editors.
@@ -29,7 +34,7 @@ public final class EditorUtils {
     private EditorUtils() {
     }
 
-    public static void openInInternalEditor(IFile file, boolean activate) {
+    public static IEditorPart openInInternalEditor(IFile file, boolean activate) {
         IEditorDescriptor desc;
         try {
             desc = IDE.getEditorDescriptor(file);
@@ -42,14 +47,15 @@ public final class EditorUtils {
 
         try {
             IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-            IDE.openEditor(activePage, file, editorId, activate);
+           return  IDE.openEditor(activePage, file, editorId, activate);
         } catch (PartInitException e) {
             String message = String.format("Cannot open file %s in editor.", file.getFullPath());
             UiPlugin.logger().error(message, e); //$NON-NLS-1$
+            throw new GradlePluginsRuntimeException(message, e);
         }
     }
 
-    public static void openInInternalEditor(File file, boolean activate) {
+    public static IEditorPart openInInternalEditor(File file, boolean activate) {
         IEditorDescriptor desc;
         try {
             desc = IDE.getEditorDescriptor(file.getName());
@@ -62,10 +68,11 @@ public final class EditorUtils {
 
         try {
             IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-            IDE.openEditor(activePage, file.toURI(), editorId, activate);
+            return IDE.openEditor(activePage, file.toURI(), editorId, activate);
         } catch (PartInitException e) {
             String message = String.format("Cannot open file %s in editor.", file.getAbsolutePath());
             UiPlugin.logger().error(message, e); //$NON-NLS-1$
+            throw new GradlePluginsRuntimeException(message, e);
         }
     }
 
@@ -78,5 +85,40 @@ public final class EditorUtils {
         }
         return editorId;
     }
+
+    public static void selectAndReveal(int offset, int length, IEditorPart editor, IFile file) {
+        if (editor instanceof ITextEditor) {
+            ITextEditor textEditor = (ITextEditor) editor;
+            textEditor.selectAndReveal(offset, length);
+        } else {
+            showWithMarker(editor, file, offset, length);
+        }
+    }
+
+    private static void showWithMarker(IEditorPart editor, IFile file, int offset, int length) {
+        IMarker marker = null;
+        try {
+            Bundle bundle = FrameworkUtil.getBundle(EditorUtils.class);
+            marker = file.createMarker(bundle.getSymbolicName() + "navigationmarker"); //$NON-NLS-1$
+            Map<String, Integer> attributes = new HashMap<String, Integer>(4);
+            attributes.put(IMarker.CHAR_START, offset);
+            attributes.put(IMarker.CHAR_END, offset + length);
+            marker.setAttributes(attributes);
+            IDE.gotoMarker(editor, marker);
+        } catch (CoreException e) {
+            String message = String.format("Cannot set marker in file %s.", file.getFullPath());
+            UiPlugin.logger().error(message, e); //$NON-NLS-1$
+            throw new GradlePluginsRuntimeException(message, e);
+        } finally {
+            if (marker != null) {
+                try {
+                    marker.delete();
+                } catch (CoreException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
 
 }
