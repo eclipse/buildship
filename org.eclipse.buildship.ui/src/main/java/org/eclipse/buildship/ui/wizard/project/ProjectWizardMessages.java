f7d629b9ae73a536375ecc6f8261a10c05d6163c
Donat Csikos <csdonat@gmail.com>
2015-06-07 16:23:18 +0200

    Show icon and info message on the preview screen when a pre-2.0 Gradle version is used for the project import
    Signed-off-by: Donat Csikos <csdonat@gmail.com>

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/wizard/project/ProjectWizardMessages.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/wizard/project/ProjectWizardMessages.java
@@ -28,6 +28,8 @@ public final class ProjectWizardMessages extends NLS {
     public static String Title_NewGradleProjectWizardPage;
     public static String Title_NewGradleProjectPreviewWizardPage;
 
+    public static String Title_Dialog_PreGradle20VersionUsed;
+
     public static String Label_ProjectRootDirectory;
     public static String Label_GradleUserHome;
     public static String Label_JavaHome;
@@ -61,6 +63,8 @@ public final class ProjectWizardMessages extends NLS {
 
     public static String InfoMessage_NewGradleProjectWizardPageContext;
     public static String InfoMessage_NewGradleProjectPreviewWizardPageContext;
+
+    public static String InfoMessage_PreGradle20VersionUsed;
 
     static {
         // initialize resource bundle
