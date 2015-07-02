fea572447a4ee6eb5afefa4c506d9509d5c35138
Donat Csikos <csdonat@gmail.com>
2015-07-02 14:09:29 +0200

    Fix checkstyle
    Signed-off-by: Donat Csikos <csdonat@gmail.com>

--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/internal/SourceFolderUpdater.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/internal/SourceFolderUpdater.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     Etienne Studer & Donát Csikós (Gradle Inc.) - initial API and implementation and initial documentation
  */
+
 package org.eclipse.buildship.core.workspace.internal;
 
 import java.util.List;
