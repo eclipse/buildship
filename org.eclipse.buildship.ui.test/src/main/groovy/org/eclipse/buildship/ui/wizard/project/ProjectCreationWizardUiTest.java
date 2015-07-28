0cb9c7b651921627cdaaeaf79276ac4e2ba00da7
Simon Scholz <simon.scholz@vogella.com>
2015-07-28 09:42:36 +0200

    Added test to make sure that the files being created at the preview page are deleted on cancel
    Change-Id: I6408236121abdfc20b9ad7649ff7cc33cb4f83c0
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- org.eclipse.buildship.ui.test/src/main/groovy/org/eclipse/buildship/ui/wizard/project/ProjectCreationWizardUiTest.java
+++ org.eclipse.buildship.ui.test/src/main/groovy/org/eclipse/buildship/ui/wizard/project/ProjectCreationWizardUiTest.java
@@ -18,6 +18,7 @@ import java.io.File;
 
 import org.eclipse.buildship.ui.BaseSWTBotTest;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotCheckBox;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
@@ -30,6 +31,7 @@ import com.google.common.collect.FluentIterable;
 
 public class ProjectCreationWizardUiTest extends BaseSWTBotTest {
 
+    private static final String NEW_GRADLE_PROJECT_WIZARD_TITLE = "New Gradle Project";
     private static final String TEST_PROJECT_NAME = "TestProject";
 
     @Test
@@ -41,7 +43,7 @@ public class ProjectCreationWizardUiTest extends BaseSWTBotTest {
         bot.text(ProjectWizardMessages.InfoMessage_NewGradleProjectWizardPageDefault);
 
         // cancel the wizard
-        bot.button("Cancel").click();
+        bot.button(IDialogConstants.CANCEL_LABEL).click();
     }
 
     @Test
@@ -53,24 +55,24 @@ public class ProjectCreationWizardUiTest extends BaseSWTBotTest {
         assertTrue(useDefaultLocationCheckBox.isChecked());
 
         // cancel the wizard
-        bot.button("Cancel").click();
+        bot.button(IDialogConstants.CANCEL_LABEL).click();
     }
 
     @Test
-    public void creationPreviewFilesAreCreatedAndDeleted() {
+    public void previewPageFilesAreCreatedAndDeleted() {
         openGradleNewWizard();
 
-        bot.waitUntil(Conditions.shellIsActive("New Gradle Project"));
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
         bot.textWithLabel("Project name").setText(TEST_PROJECT_NAME);
 
-        bot.waitUntil(Conditions.shellIsActive("New Gradle Project"));
-        bot.button("Next >").click();
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
+        bot.button(IDialogConstants.NEXT_LABEL).click();
 
-        bot.waitUntil(Conditions.shellIsActive("New Gradle Project"));
-        bot.button("Next >").click();
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
+        bot.button(IDialogConstants.NEXT_LABEL).click();
 
         // wait at the project preview page until the "< Back" button is enabled
-        bot.waitUntil(Conditions.widgetIsEnabled(bot.button("< Back")));
+        bot.waitUntil(Conditions.widgetIsEnabled(bot.button(IDialogConstants.BACK_LABEL)));
 
         File workspaceRootDir = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
 
@@ -91,15 +93,54 @@ public class ProjectCreationWizardUiTest extends BaseSWTBotTest {
         assertTrue(FluentIterable.of(testProjectFiles).anyMatch(new FileNamePredicate("build.gradle")));
         assertTrue(FluentIterable.of(testProjectFiles).anyMatch(new FileNamePredicate("settings.gradle")));
 
-        bot.button("< Back").click();
+        bot.button(IDialogConstants.BACK_LABEL).click();
 
         // make sure that the "TestProject" is deleted in case of clicking back
         // in the wizard
         assertFalse(testProjectDir.exists());
 
         // cancel the wizard
-        bot.button("Cancel").click();
+        bot.button(IDialogConstants.CANCEL_LABEL).click();
     }
+
+    @Test
+    public void previewPageFilesAreDeletedOnCancel() {
+        openGradleNewWizard();
+
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
+        bot.textWithLabel("Project name").setText(TEST_PROJECT_NAME);
+
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
+        bot.button(IDialogConstants.NEXT_LABEL).click();
+
+        bot.waitUntil(Conditions.shellIsActive(NEW_GRADLE_PROJECT_WIZARD_TITLE));
+        bot.button(IDialogConstants.NEXT_LABEL).click();
+
+        // wait at the project preview page until the "< Back" button is enabled
+        bot.waitUntil(Conditions.widgetIsEnabled(bot.button(IDialogConstants.BACK_LABEL)));
+
+        File workspaceRootDir = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
+
+        assertTrue(workspaceRootDir.isDirectory());
+
+        File[] workspaceRootFiles = workspaceRootDir.listFiles();
+        Optional<File> firstMatch = FluentIterable.of(workspaceRootFiles)
+                .firstMatch(new FileNamePredicate(TEST_PROJECT_NAME));
+
+        // check if the "TestProject" has been created
+        assertTrue(firstMatch.isPresent());
+        File testProjectDir = firstMatch.get();
+        assertTrue(testProjectDir.isDirectory());
+
+
+        // cancel the wizard
+        bot.button(IDialogConstants.CANCEL_LABEL).click();
+
+        // make sure that the "TestProject" is deleted in case of clicking
+        // cancel in the wizard
+        assertFalse(testProjectDir.exists());
+    }
+
 
     private void openGradleNewWizard() {
         bot.menu("File").menu("New").menu("Other...").click();
@@ -107,7 +148,7 @@ public class ProjectCreationWizardUiTest extends BaseSWTBotTest {
         shell.activate();
         bot.waitUntil(Conditions.shellIsActive("New"));
         bot.tree().expandNode("Gradle").select("Gradle Project");
-        bot.button("Next >").click();
+        bot.button(IDialogConstants.NEXT_LABEL).click();
     }
 
     public static class FileNamePredicate implements Predicate<File> {
