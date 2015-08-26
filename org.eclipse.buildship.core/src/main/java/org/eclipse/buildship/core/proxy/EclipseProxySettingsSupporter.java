4e79dfa5d7b5dcda43c022832dce2392d8093f72
Ian Stewart-Binks <istewart@redhat.com>
2015-08-26 16:43:10 -0400

    Clean up proxy supporter
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/proxy/EclipseProxySettingsSupporter.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/proxy/EclipseProxySettingsSupporter.java
@@ -20,75 +20,33 @@ import org.eclipse.buildship.core.CorePlugin;
 public class EclipseProxySettingsSupporter {
 
     String savedHTTPProxyHost, savedHTTPProxyPort, savedHTTPProxyUser, savedHTTPProxyPassword;
-    String savedHTTPSProxyHost, savedHTTPSProxyPort, savedHTTPSProxyUser, savedHTTPSProxyPassword;
 
     public void configureEclipseProxySettings() {
         configureHTTPProxySettings();
-        configureHTTPSProxySettings();
     }
-
-    public void storeSystemProxySettings() {
-        this.savedHTTPProxyHost = System.getProperty("http.proxyHost");
-        this.savedHTTPProxyPort = System.getProperty("http.proxyPort");
-        this.savedHTTPProxyUser = System.getProperty("http.proxyUser");
-        this.savedHTTPProxyPassword = System.getProperty("http.proxyPassword");
-
-        this.savedHTTPSProxyHost = System.getProperty("https.proxyHost");
-        this.savedHTTPSProxyPort = System.getProperty("https.proxyPort");
-        this.savedHTTPSProxyUser = System.getProperty("https.proxyUser");
-        this.savedHTTPSProxyPassword = System.getProperty("https.proxyPassword");
-    }
-
 
     public void restoreSystemProxySettings() {
         resetOrClearSystemProperty("http.proxyHost", this.savedHTTPProxyHost);
         resetOrClearSystemProperty("http.proxyPort", this.savedHTTPProxyPort);
         resetOrClearSystemProperty("http.proxyUser", this.savedHTTPProxyUser);
         resetOrClearSystemProperty("http.proxyPassword", this.savedHTTPProxyPassword);
-
-        resetOrClearSystemProperty("https.proxyHost", this.savedHTTPSProxyHost);
-        resetOrClearSystemProperty("https.proxyPort", this.savedHTTPSProxyPort);
-        resetOrClearSystemProperty("https.proxyUser", this.savedHTTPSProxyUser);
-        resetOrClearSystemProperty("https.proxyPassword", this.savedHTTPSProxyPassword);
     }
 
     private void configureHTTPProxySettings() {
         Optional<IProxyData> httpProxyData = Optional.of(CorePlugin.getProxyService().getProxyData(IProxyData.HTTP_PROXY_TYPE));
         if (httpProxyData.isPresent()) {
             if (httpProxyData.get().getHost() != null) {
-                System.setProperty("http.proxyHost", httpProxyData.get().getHost());
-            } else {
-                System.setProperty("http.proxyHost", "value");
+                this.savedHTTPProxyHost = System.setProperty("http.proxyHost", httpProxyData.get().getHost());
             }
             if (httpProxyData.get().getPort() != -1) {
-                System.setProperty("http.proxyPort", Integer.toString(httpProxyData.get().getPort()));
+                this.savedHTTPProxyPort = System.setProperty("http.proxyPort", Integer.toString(httpProxyData.get().getPort()));
             }
             if (httpProxyData.get().getUserId() != null) {
-                System.setProperty("http.proxyUser", httpProxyData.get().getUserId());
+                this.savedHTTPProxyUser = System.setProperty("http.proxyUser", httpProxyData.get().getUserId());
             }
             if (httpProxyData.get().getPassword() != null) {
-                System.setProperty("http.proxyPassword", httpProxyData.get().getPassword());
+                this.savedHTTPProxyPassword = System.setProperty("http.proxyPassword", httpProxyData.get().getPassword());
             }
-            // System.setProperty("http.nonProxyHosts", CorePlugin.getProxyService().getNonProxiedHosts());
-        }
-    }
-
-    private void configureHTTPSProxySettings() {
-        Optional<IProxyData> httpsProxyData = Optional.of(CorePlugin.getProxyService().getProxyData(IProxyData.HTTPS_PROXY_TYPE));
-        if (httpsProxyData.isPresent()) {
-            if (httpsProxyData.get().getHost() != null) {
-                System.setProperty("https.proxyHost", httpsProxyData.get().getHost());
-            }
-            if (httpsProxyData.get().getPort() != -1) {
-                System.setProperty("https.proxyPort", Integer.toString(httpsProxyData.get().getPort()));
-            }
-            if (httpsProxyData.get().getUserId() != null) {
-                System.setProperty("https.proxyUser", httpsProxyData.get().getUserId());
-            }
-            if (httpsProxyData.get().getPassword() != null) {
-                System.setProperty("https.proxyPassword", httpsProxyData.get().getPassword());
-            }
-            // System.setProperty("http.nonProxyHosts", CorePlugin.getProxyService().getNonProxiedHosts());
         }
     }
 
