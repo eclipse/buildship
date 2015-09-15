747d67b1f35eabdb5e75b7641bdaba5738bcf41c
etiennestuder <etienne@studer.nu>
2015-09-15 12:25:44 +0200

    Fix tests since .settings folder is now not deleted anymore when un-linking a Gradle project

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/RefreshGradleProjectJobTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/RefreshGradleProjectJobTest.groovy
@@ -1,12 +1,8 @@
 package org.eclipse.buildship.core.workspace
 
-import java.io.File
-import java.util.List
-
 import org.eclipse.core.resources.IProject
 import org.eclipse.core.resources.IProjectDescription
 import org.eclipse.core.resources.IResource
-import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.core.runtime.NullProgressMonitor
 import org.eclipse.jdt.core.JavaCore
 
@@ -34,7 +30,7 @@ class RefreshGradleProjectJobTest extends ProjectImportSpecification {
         JavaCore.create(findProject('moduleA')).resolvedClasspath.find{ it.path.toPortableString().endsWith('junit-4.12.jar') }
     }
 
-    def "Artifacts are discarded when the project is excluded from a Gradle build"() {
+    def "Gradle nature and Gradle settings file are discarded when the project is excluded from a Gradle build"() {
         setup:
         file('sample', 'settings.gradle').text = """
            include 'moduleA'
@@ -48,7 +44,8 @@ class RefreshGradleProjectJobTest extends ProjectImportSpecification {
         IProject project = findProject('moduleB')
         project != null
         !GradleProjectNature.INSTANCE.isPresentOn(project)
-        !project.getFolder('.settings').exists()
+        !project.getFolder('.settings').getFile('gradle.prefs').exists()
+        project.getFolder('.settings').exists()
     }
 
     def "A new Gradle module is imported into the workspace"() {
