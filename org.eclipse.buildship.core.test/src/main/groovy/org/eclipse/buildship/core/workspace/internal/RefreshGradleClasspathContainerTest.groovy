cbf5413c2a3688ff5ccefaf16a7393a5061ead48
Donat Csikos <csdonat@gmail.com>
2015-09-23 11:12:13 +0200

    Re-enable RefreshGradleClasspathContainerTest

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/RefreshGradleClasspathContainerTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/RefreshGradleClasspathContainerTest.groovy
@@ -5,14 +5,11 @@ import org.eclipse.buildship.core.test.fixtures.ProjectImportSpecification
 import org.eclipse.buildship.core.workspace.GradleClasspathContainer
 import org.eclipse.buildship.core.workspace.SynchronizeGradleProjectsJob
 
-import spock.lang.Ignore;
-
 import org.eclipse.core.runtime.Path
 import org.eclipse.jdt.core.IClasspathContainer
 import org.eclipse.jdt.core.IJavaProject
 import org.eclipse.jdt.core.JavaCore
 
-@Ignore
 class RefreshGradleClasspathContainerTest extends ProjectImportSpecification {
 
     def "Update the project classpath"() {
