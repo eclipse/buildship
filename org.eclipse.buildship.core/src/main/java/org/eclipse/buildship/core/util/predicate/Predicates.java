1107094cfed81b8412ab2bea310b354db9ea0acf
Simon Scholz <simon.scholz@vogella.com>
2015-10-15 11:30:40 +0200

    Bug 478530 - Add option to disable-enable build folder filtering
    Change-Id: Ie3e2f256dae25abeba8b1470f574458dedfa5662
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/util/predicate/Predicates.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/util/predicate/Predicates.java
@@ -11,11 +11,20 @@
 
 package org.eclipse.buildship.core.util.predicate;
 
-import com.google.common.base.Predicate;
+import java.util.List;
+
+import org.eclipse.buildship.core.CorePlugin;
 import org.eclipse.buildship.core.GradlePluginsRuntimeException;
 import org.eclipse.buildship.core.configuration.GradleProjectNature;
+import org.eclipse.buildship.core.workspace.internal.ResourceFilter;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 
 /**
  * Supplies some useful {@link Predicate} instances.
@@ -23,6 +32,74 @@ import org.eclipse.core.runtime.CoreException;
 public final class Predicates {
 
     private Predicates() {
+    }
+
+    private static final Predicate<IResource> isChildResourcesVisible = new Predicate<IResource>() {
+
+        @Override
+        public boolean apply(IResource resource) {
+            return isResourceFiltered(resource, ResourceFilter.CHILD_LOCATIONS_FILTER_RESOURCES_QUALIFIEDNAME);
+        }
+    };
+
+    private static final Predicate<IResource> isBuildFolderVisible = new Predicate<IResource>() {
+
+        @Override
+        public boolean apply(IResource resource) {
+            return isResourceFiltered(resource, ResourceFilter.FILTER_BUILD_FOLDER_QUALIFIEDNAME);
+        }
+    };
+
+    private static final Predicate<IResource> isGradleFolderVisible = new Predicate<IResource>() {
+
+        @Override
+        public boolean apply(IResource resource) {
+            return isResourceFiltered(resource, ResourceFilter.FILTER_GRADLE_FOLDER_QUALIFIEDNAME);
+        }
+    };
+
+    private static final Predicate<IResource> isDotGradleFolderVisible = new Predicate<IResource>() {
+
+        @Override
+        public boolean apply(IResource resource) {
+            return isResourceFiltered(resource, ResourceFilter.FILTER_DOT_GRADLE_FOLDER_QUALIFIEDNAME);
+        }
+    };
+
+    public static Predicate<IResource> isChildResourcesVisible() {
+        return isChildResourcesVisible;
+    }
+
+    public static Predicate<IResource> isBuildFolderVisible() {
+        return isBuildFolderVisible;
+    }
+
+    public static Predicate<IResource> isGradleFolderVisible() {
+        return isGradleFolderVisible;
+    }
+
+    public static Predicate<IResource> isDotGradleFolderVisible() {
+        return isDotGradleFolderVisible;
+    }
+
+    private static boolean isResourceFiltered(IResource resource, QualifiedName persistentProperty) {
+        List<String> filteredLocations = getLocationsToBeFiltered(resource, persistentProperty);
+        return !filteredLocations.contains(resource.getLocation().toOSString());
+    }
+
+    private static List<String> getLocationsToBeFiltered(IResource resource, QualifiedName persistentProperty) {
+        try {
+            String resourceLocationsProperty = resource.getProject().getPersistentProperty(persistentProperty);
+            if (resourceLocationsProperty != null) {
+                return Splitter.on(ResourceFilter.FILTER_ELEMENT_SEPARATOR).trimResults().omitEmptyStrings()
+                        .splitToList(resourceLocationsProperty);
+            }
+        } catch (CoreException e) {
+            // just log and return the empty list
+            CorePlugin.logger().info(e.getMessage(), e);
+        }
+
+        return ImmutableList.of();
     }
 
     public static Predicate<IProject> accessibleGradleProject() {
--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/internal/ResourceFilter.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/internal/ResourceFilter.java
@@ -15,35 +15,41 @@ import java.io.File;
 import java.util.List;
 import java.util.Set;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-
+import org.eclipse.buildship.core.CorePlugin;
+import org.eclipse.buildship.core.GradlePluginsRuntimeException;
+import org.eclipse.buildship.core.util.object.MoreObjects;
 import org.eclipse.core.resources.FileInfoMatcherDescription;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceFilterDescription;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.QualifiedName;
 
-import org.eclipse.buildship.core.GradlePluginsRuntimeException;
-import org.eclipse.buildship.core.util.object.MoreObjects;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
 
 /**
  * Provides resource filtering on {@link IProject} instances.
  */
-final class ResourceFilter {
+public final class ResourceFilter {
 
-    // to create filters we reuse the constants and classes from the org.eclipse.core.resources
-    // plug-in. The IDs are not exposed as a class, consequently we redefine them here.
-    // Documentation:
-    // http://help.eclipse.org/luna/topic/org.eclipse.platform.doc.isv/guide/resInt_filters.htm
+    public static final char FILTER_ELEMENT_SEPARATOR = ',';
 
-    // resource filter id
-    private static final String FILTER_ID = "org.eclipse.ui.ide.multiFilter"; //$NON-NLS-1$
+    public static final QualifiedName CHILD_LOCATIONS_FILTER_RESOURCES_QUALIFIEDNAME = new QualifiedName(
+            CorePlugin.PLUGIN_ID, "filteredChildResources"); //$NON-NLS-1$
+
+    public static final QualifiedName FILTER_BUILD_FOLDER_QUALIFIEDNAME = new QualifiedName(CorePlugin.PLUGIN_ID,
+            "filteredBuildfolder"); //$NON-NLS-1$
+
+    public static final QualifiedName FILTER_GRADLE_FOLDER_QUALIFIEDNAME = new QualifiedName(CorePlugin.PLUGIN_ID,
+            "filteredGradleFolder"); //$NON-NLS-1$
+
+    public static final QualifiedName FILTER_DOT_GRADLE_FOLDER_QUALIFIEDNAME = new QualifiedName(CorePlugin.PLUGIN_ID,
+            "filteredDotGradleFolder"); //$NON-NLS-1$
 
     // properties key to store/retrieve the filters created by Buildship
     private static final String PROJECT_PROPERTY_KEY_GRADLE_FILTERS = "GRADLE_FILTERS";
@@ -52,33 +58,44 @@ final class ResourceFilter {
     }
 
     /**
-     * Attaches resource filters on the specified project to hide any of the given child locations.
+     * Attaches resource filters on the specified project to hide any of the
+     * given child locations.
      *
-     * @param project the project for which to create resource filters
-     * @param childLocations the child locations
-     * @param monitor the monitor to report progress on
+     * @param project
+     *            the project for which to create resource filters
+     * @param childLocations
+     *            the child locations
+     * @param monitor
+     *            the monitor to report progress on
+     * @throws CoreException
      */
-    public static void attachFilters(IProject project, List<File> childLocations, IProgressMonitor monitor) {
+    public static void attachFilters(IProject project, List<File> childLocations, IProgressMonitor monitor)
+            throws CoreException {
         monitor = MoreObjects.firstNonNull(monitor, new NullProgressMonitor());
-        List<FileInfoMatcherDescription> matchers = createMatchers(project, childLocations);
-        setExclusionFilters(project, matchers, monitor);
+        addFilterValuesToProject(project, childLocations, monitor);
     }
 
     /**
-     * Removes all resource filters created with the {@link #attachFilters(IProject, List, IProgressMonitor)} method.
+     * Removes all resource filters created with the
+     * {@link #attachFilters(IProject, List, IProgressMonitor)} method.
      *
-     * @param project the target project to remove the filters from
-     * @param monitor the monitor to report progress on
+     * @param project
+     *            the target project to remove the filters from
+     * @param monitor
+     *            the monitor to report progress on
      */
     public static void detachAllFilters(IProject project, IProgressMonitor monitor) {
         monitor = MoreObjects.firstNonNull(monitor, new NullProgressMonitor());
-        monitor.beginTask(String.format("Remove Gradle resource filters from project %s", project), IProgressMonitor.UNKNOWN);
+        monitor.beginTask(String.format("Remove Gradle resource filters from project %s", project),
+                IProgressMonitor.UNKNOWN);
         try {
-            StringSetProjectProperty knownMatcherNames = StringSetProjectProperty.from(project, PROJECT_PROPERTY_KEY_GRADLE_FILTERS);
+            StringSetProjectProperty knownMatcherNames = StringSetProjectProperty.from(project,
+                    PROJECT_PROPERTY_KEY_GRADLE_FILTERS);
             Set<String> matcherNames = knownMatcherNames.get();
             for (IResourceFilterDescription filter : project.getFilters()) {
                 FileInfoMatcherDescription matcher = filter.getFileInfoMatcherDescription();
-                if (matcher != null && matcher.getArguments() instanceof String && matcherNames.contains(matcher.getArguments())) {
+                if (matcher != null && matcher.getArguments() instanceof String
+                        && matcherNames.contains(matcher.getArguments())) {
                     knownMatcherNames.remove((String) matcher.getArguments());
                     filter.delete(IResource.NONE, monitor);
                 }
@@ -90,64 +107,30 @@ final class ResourceFilter {
         }
     }
 
-    static List<FileInfoMatcherDescription> createMatchers(IProject project, List<File> children) {
-        ImmutableList.Builder<FileInfoMatcherDescription> matchers = ImmutableList.builder();
-        IPath projectLocation = project.getLocation();
-        for (File child : children) {
-            IPath childLocation = new Path(child.getAbsolutePath());
-            if (projectLocation.isPrefixOf(childLocation)) {
-                matchers.add(new FileInfoMatcherDescription(FILTER_ID, createMultiFilterArgument(childLocation.makeRelativeTo(projectLocation).toPortableString())));
-            }
-        }
-        return matchers.build();
-    }
-
-    private static String createMultiFilterArgument(String relativeLocation) {
-        return "1.0-projectRelativePath-matches-false-false-" + relativeLocation; //$NON-NLS-1$
-    }
-
-    private static void setExclusionFilters(IProject project, List<FileInfoMatcherDescription> matchers, IProgressMonitor monitor) {
-        monitor.beginTask(String.format("Set Gradle resource filters for project %s", project), IProgressMonitor.UNKNOWN); //$NON-NLS-1$
-        try {
-            // retrieve already defined filters
-            final IResourceFilterDescription[] existingFilters;
-            try {
-                existingFilters = project.getFilters();
-            } catch (CoreException e) {
-                String message = String.format("Cannot retrieve current resource filters for project %s.", project.getName()); //$NON-NLS-1$
-                throw new GradlePluginsRuntimeException(message, e);
-            }
-
-            // filter the matchers by removing all matchers for which there is already a filter defined on the project
-            ImmutableList<FileInfoMatcherDescription> newMatchers = FluentIterable.from(matchers).filter(new Predicate<FileInfoMatcherDescription>() {
-
-                @Override
-                public boolean apply(FileInfoMatcherDescription matcher) {
-                    for (IResourceFilterDescription existingFilter : existingFilters) {
-                        if (existingFilter.getFileInfoMatcherDescription().equals(matcher)) {
-                            return false;
-                        }
+    private static void addFilterValuesToProject(IProject project, List<File> childLocations, IProgressMonitor monitor)
+            throws CoreException {
+        ImmutableList<String> childLocationStrings = FluentIterable.from(childLocations)
+                .transform(new Function<File, String>() {
+                    @Override
+                    public String apply(File input) {
+                        return input.getAbsolutePath();
                     }
-                    return true;
-                }
-            }).toList();
+                }).toList();
 
-            // create new filters for the new matchers and assign them to the project
-            if (!matchers.isEmpty()) {
-                try {
-                    int type = IResourceFilterDescription.EXCLUDE_ALL | IResourceFilterDescription.FOLDERS | IResourceFilterDescription.INHERITABLE;
-                    for (FileInfoMatcherDescription matcher : newMatchers) {
-                        project.createFilter(type, matcher, IResource.NONE, new NullProgressMonitor());
-                        StringSetProjectProperty.from(project, PROJECT_PROPERTY_KEY_GRADLE_FILTERS).add((String) matcher.getArguments());
-                    }
-                } catch (CoreException e) {
-                    String message = String.format("Cannot create new resource filters for project %s.", project); //$NON-NLS-1$
-                    throw new GradlePluginsRuntimeException(message, e);
-                }
-            }
-        } finally {
-            monitor.done();
-        }
+        // store filtered child locations as persistentProperty in the project
+        // resource, which can be read by an AbstractGradleViewerFilter for
+        // filtering
+        String joinedChildLocations = Joiner.on(FILTER_ELEMENT_SEPARATOR).skipNulls().join(childLocationStrings);
+        project.setPersistentProperty(CHILD_LOCATIONS_FILTER_RESOURCES_QUALIFIEDNAME, joinedChildLocations);
+
+        // TODO the build and .gradle values are currently hard codes. This
+        // should be replaced by values coming from the TAPI.
+        project.setPersistentProperty(FILTER_BUILD_FOLDER_QUALIFIEDNAME,
+                project.getLocation().append("build").toOSString());
+        project.setPersistentProperty(FILTER_GRADLE_FOLDER_QUALIFIEDNAME,
+                project.getLocation().append("gradle").toOSString());
+        project.setPersistentProperty(FILTER_DOT_GRADLE_FOLDER_QUALIFIEDNAME,
+                project.getLocation().append(".gradle").toOSString());
     }
 
 }
