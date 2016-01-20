842bcf921b95ab264ffeedbd76aef4a0b92e3f8b
Donat Csikos <csdonat@gmail.com>
2016-01-20 10:31:14 +0100

    Remove println

--- buildSrc/src/main/groovy/eclipsebuild/BuildDefinitionPlugin.groovy
+++ buildSrc/src/main/groovy/eclipsebuild/BuildDefinitionPlugin.groovy
@@ -249,7 +249,6 @@ class BuildDefinitionPlugin implements Plugin<Project> {
             updateSites.add(location.repository.@location.text().replace('\${project_loc}', 'file://' +  project.projectDir.absolutePath))
             location.unit.each {unit -> features.add("${unit.@id}/${unit.@version}") }
         }
-        println updateSites
 
         // invoke the P2 director application to assemble install all features from the target
         // definition file to the target platform: http://help.eclipse.org/luna/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Fguide%2Fp2_director.html
