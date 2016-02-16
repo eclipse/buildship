5f97c1b21d0f70fa189d5311e50166fc42fb9d15
Donat Csikos <csdonat@gmail.com>
2016-02-16 11:02:11 +0100

    Adjust STS notification title

--- org.eclipse.buildship.stsmigration/src/main/java/org/eclipse/buildship/stsmigration/internal/StsMigrationDialog.java
+++ org.eclipse.buildship.stsmigration/src/main/java/org/eclipse/buildship/stsmigration/internal/StsMigrationDialog.java
@@ -40,7 +40,7 @@ import org.eclipse.ui.PlatformUI;
 class StsMigrationDialog extends Dialog {
 
     private static final String STS_MIGRATION_DOCUMENT_URL = "https://github.com/eclipse/buildship/wiki/Migration-guide-from-STS-to-Buildship";
-    private static final String DIALOG_TITLE = "Migration from SpringSource Gradle to Buildship";
+    private static final String DIALOG_TITLE = "Migration from STS Gradle to Buildship";
     private static final String DIALOG_TEXT = "The development of the STS Gradle plugin is discontinued.\n\nA document "
             + "explaining the migration process from SpringSource Gradle to Buildship can be found <a>here</a>.";
     private static final String MUTE_NOTIFICATION_TEXT = "Don't show this message again";
