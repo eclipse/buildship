270e573565c298d0c3f18a67b5b937be746a20f1
Rene Groeschke <rene@gradle.com>
2016-02-24 23:08:00 +0000

    try fix git tagging script

--- tag.sh
+++ tag.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export GIT_SSH="ssh -i ~/.ssh/id_rsa_gradlewaregitbot_buildship"
+export GIT_SSH_COMMAND="ssh -i ssh -i ~/.ssh/id_rsa_gradlewaregitbot_buildship"
 git tag -a REL_$1 -m"Create tag REL_$1"
 git push origin REL_$1
