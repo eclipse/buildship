c6939e8f7733336d8d23554f366c73e092ea79e9
Stefan Oehme <st.oehme@gmail.com>
2016-03-03 14:56:24 -0500

    Simplify test by extracting a data class
    The test was passing around half a dozen fields, most of which were
    always the same. Replaced those fields with a simple class and a default
    instance of that class. Test scenarios are now constructed by copying
    and modifying the defaults.

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/launch/GradleRunConfigurationAttributesTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/launch/GradleRunConfigurationAttributesTest.groovy
@@ -15,83 +15,97 @@ import org.eclipse.debug.core.DebugPlugin
 import org.eclipse.buildship.core.GradlePluginsRuntimeException;
 import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper;
 import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper.DistributionType
+
+import java.lang.reflect.Field;
+
+import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
+import groovy.transform.Canonical
+import groovy.transform.stc.ClosureParams;
+import groovy.transform.stc.FirstParam;
 import spock.lang.Shared;
 import spock.lang.Specification;
 
 class GradleRunConfigurationAttributesTest extends Specification {
 
-    // list of valid attributes
-    @Shared def atr = [
-        'tasks' : ['clean'],
-        'workingDir' : "/home/user/workspace",
-        'gradleDistr' : GradleDistributionWrapper.from(DistributionType.WRAPPER, null).toGradleDistribution(),
-        'gradleHome' : "/.gradle",
-        'javaHome' : "/.java",
-        'arguments' : ["-q"],
-        'jvmArguments' : ["-ea"],
-        'showExecutionView' :  true,
-        'showConsoleView' : true
-    ]
+    @Shared def Attributes defaults = new Attributes (
+        tasks : ['clean'],
+        workingDir : "/home/user/workspace",
+        gradleDistr : GradleDistributionWrapper.from(DistributionType.WRAPPER, null).toGradleDistribution(),
+        gradleHome : "/.gradle",
+        javaHome : "/.java",
+        arguments : ["-q"],
+        jvmArguments : ["-ea"],
+        showExecutionView :  true,
+        showConsoleView : true
+    )
 
     def "Can create a new valid instance"() {
         when:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, atr.javaHome, atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def configuration = defaults.toConfiguration()
 
         then:
         // not null
         configuration != null
         // check non-calculated values
-        configuration.getTasks() == atr.tasks
-        configuration.getWorkingDirExpression() == atr.workingDir
-        configuration.getGradleDistribution() == atr.gradleDistr
-        configuration.getGradleUserHomeExpression() == atr.gradleHome
-        configuration.getJavaHomeExpression() == atr.javaHome
-        configuration.getJvmArguments() == atr.jvmArguments
-        configuration.getArguments() == atr.arguments
-        configuration.isShowExecutionView() == atr.showExecutionView
-        configuration.isShowConsoleView() == atr.showConsoleView
+        configuration.getTasks() == defaults.tasks
+        configuration.getWorkingDirExpression() == defaults.workingDir
+        configuration.getGradleDistribution() == defaults.gradleDistr
+        configuration.getGradleUserHomeExpression() == defaults.gradleHome
+        configuration.getJavaHomeExpression() == defaults.javaHome
+        configuration.getJvmArguments() == defaults.jvmArguments
+        configuration.getArguments() == defaults.arguments
+        configuration.isShowExecutionView() == defaults.showExecutionView
+        configuration.isShowConsoleView() == defaults.showConsoleView
         // check calculated value
-        configuration.getArgumentExpressions() == atr.arguments
-        configuration.getJvmArgumentExpressions() == atr.jvmArguments
-        configuration.getWorkingDir().getAbsolutePath() == new File(atr.workingDir).getAbsolutePath()
-        configuration.getGradleUserHome().getAbsolutePath() == new File(atr.gradleHome).getAbsolutePath()
-        configuration.getJavaHome().getAbsolutePath() == new File(atr.javaHome).getAbsolutePath()
+        configuration.getArgumentExpressions() == defaults.arguments
+        configuration.getJvmArgumentExpressions() == defaults.jvmArguments
+        configuration.getWorkingDir().getAbsolutePath() == new File(defaults.workingDir).getAbsolutePath()
+        configuration.getGradleUserHome().getAbsolutePath() == new File(defaults.gradleHome).getAbsolutePath()
+        configuration.getJavaHome().getAbsolutePath() == new File(defaults.javaHome).getAbsolutePath()
     }
 
-    def "Can create a new valid instance with valid null arguments"() {
+    def "Can create a new valid instance with valid null arguments"(Attributes attributes) {
         when:
-        def configuration = GradleRunConfigurationAttributes.with(tasks, workingDir, gradleDistr, gradleHome, javaHome, jvmArguments, arguments, showExecutionView, showConsoleView)
+        def configuration = attributes.toConfiguration()
 
         then:
         configuration != null
-        gradleHome != null || configuration.getGradleUserHome() == null
-        javaHome != null || configuration.getJavaHome() == null
+        attributes.gradleHome != null || configuration.getGradleUserHome() == null
+        attributes.javaHome != null || configuration.getJavaHome() == null
 
         where:
-        tasks     | workingDir     | gradleDistr     | gradleHome     | javaHome     | jvmArguments     | arguments     | showExecutionView     | showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | null           | atr.javaHome | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | atr.gradleHome | null         | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | null           | null         | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
+        attributes << [
+            defaults.copy { gradleHome = null },
+            defaults.copy { javaHome = null },
+            defaults.copy { gradleHome = null; javaHome = null }
+        ]
     }
 
-    def "Creation fails when null argument passed"() {
+    def "Creation fails when null argument passed"(Attributes attributes) {
         when:
-        GradleRunConfigurationAttributes.with(tasks, workingDir, gradleDistr, gradleHome, javaHome, jvmArguments, arguments, showExecutionView, showConsoleView)
+        attributes.toConfiguration()
 
         then:
         thrown(RuntimeException)
 
         where:
-        tasks     | workingDir     | gradleDistr     | gradleHome     | javaHome     | jvmArguments     | arguments     | showExecutionView     | showConsoleView
-        null      | atr.workingDir | atr.gradleDistr | atr.gradleHome | atr.javaHome | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | null           | atr.gradleDistr | atr.gradleHome | atr.javaHome | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | atr.gradleHome | atr.javaHome | null             | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | atr.gradleHome | atr.javaHome | atr.jvmArguments | null          | atr.showExecutionView | atr.showConsoleView
+        attributes << [
+            defaults.copy { tasks = null },
+            defaults.copy { workingDir = null },
+            defaults.copy { jvmArguments = null},
+            defaults.copy { arguments = null}
+        ]
     }
 
     def "Expressions can be resolved in the parameters"() {
         when:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, '${workspace_loc}/working_dir', atr.gradleDistr, '${workspace_loc}/gradle_user_home', '${workspace_loc}/java_home', atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            workingDir = '${workspace_loc}/working_dir'
+            gradleHome = '${workspace_loc}/gradle_user_home'
+            javaHome = '${workspace_loc}/java_home'
+        }
+        def configuration = attributes.toConfiguration()
 
         then:
         configuration.getWorkingDir().getPath().endsWith("working_dir")
@@ -104,7 +118,10 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
     def "Unresolvable expressions in Java home results in runtime exception"() {
         setup:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, '${nonexistingvariable}/java_home', atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            javaHome = '${nonexistingvariable}/java_home'
+        }
+        def configuration = attributes.toConfiguration()
 
         when:
         configuration.getJavaHome()
@@ -116,7 +133,10 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
     def "Unresolvable expressions in Gradle user home results in runtime exception"() {
         setup:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, '${nonexistingvariable}/gradle_user_home', atr.javaHome, atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            gradleHome = '${nonexistingvariable}/gradle_user_home'
+        }
+        def configuration = attributes.toConfiguration()
 
         when:
         configuration.getGradleUserHome()
@@ -128,7 +148,10 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
     def "Unresolvable expressions in working directory results in runtime exception"() {
         setup:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, '${nonexistingvariable}/working_dir', atr.gradleDistr, atr.gradleHome, atr.javaHome, atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            workingDir = '${nonexistingvariable}/working_dir'
+        }
+        def configuration = attributes.toConfiguration()
 
         when:
         configuration.getWorkingDir()
@@ -139,7 +162,10 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
     def "Unresolvable expressions in arguments results in runtime exception"() {
         setup:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, atr.javaHome, atr.jvmArguments,  ['${nonexistingvariable}/arguments'], atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            arguments = ['${nonexistingvariable}/arguments']
+        }
+        def configuration = attributes.toConfiguration()
 
         when:
         configuration.getArguments()
@@ -150,7 +176,10 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
     def "Unresolvable expressions in jvm arguments results in runtime exception"() {
         setup:
-        def configuration = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, atr.javaHome,  ['${nonexistingvariable}/jvmarguments'],  atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def Attributes attributes = defaults.copy {
+            jvmArguments = ['${nonexistingvariable}/jvmarguments']
+        }
+        def configuration = attributes.toConfiguration()
 
         when:
         configuration.getJvmArguments()
@@ -167,21 +196,21 @@ class GradleRunConfigurationAttributesTest extends Specification {
 
         when:
         assert eclipseConfig.getAttributes().isEmpty()
-        def gradleConfig = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, atr.javaHome, atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def gradleConfig = defaults.toConfiguration()
         gradleConfig.apply(eclipseConfig)
 
         then:
-        eclipseConfig.getAttributes().size() == atr.size()
+        eclipseConfig.getAttributes().size() == defaults.size()
     }
 
-    def "All valid configuration settings can be stored and retrieved"() {
+    def "All valid configuration settings can be stored and retrieved"(Attributes attributes) {
         setup:
         def launchManager = DebugPlugin.getDefault().getLaunchManager();
         def type = launchManager.getLaunchConfigurationType(GradleRunConfigurationDelegate.ID);
         def eclipseConfig = type.newInstance(null, "launch-config-name")
 
         when:
-        def gradleConfig1 = GradleRunConfigurationAttributes.with(tasks, workingDir, gradleDistr, gradleHome, javaHome, jvmArguments, arguments, showExecutionView, showConsoleView)
+        def gradleConfig1 = attributes.toConfiguration()
         gradleConfig1.apply(eclipseConfig)
         def gradleConfig2 = GradleRunConfigurationAttributes.from(eclipseConfig)
 
@@ -196,11 +225,12 @@ class GradleRunConfigurationAttributesTest extends Specification {
         gradleConfig1.isShowExecutionView() == gradleConfig2.isShowExecutionView()
 
         where:
-        tasks     | workingDir     | gradleDistr     | gradleHome     | javaHome     | jvmArguments     | arguments     | showExecutionView     | showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | atr.gradleHome | atr.javaHome | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | null           | atr.javaHome | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | atr.gradleHome | null         | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
-        atr.tasks | atr.workingDir | atr.gradleDistr | null           | null         | atr.jvmArguments | atr.arguments | atr.showExecutionView | atr.showConsoleView
+        attributes << [
+            defaults,
+            defaults.copy { gradleHome = null },
+            defaults.copy { javaHome = null },
+            defaults.copy { gradleHome = null; javaHome = null }
+        ]
     }
 
     def "Saved Configuration attributes has same unique attributes"() {
@@ -210,11 +240,40 @@ class GradleRunConfigurationAttributesTest extends Specification {
         def eclipseConfig = type.newInstance(null, "launch-config-name")
 
         when:
-        def gradleConfig = GradleRunConfigurationAttributes.with(atr.tasks, atr.workingDir, atr.gradleDistr, atr.gradleHome, atr.javaHome, atr.jvmArguments, atr.arguments, atr.showExecutionView, atr.showConsoleView)
+        def gradleConfig = defaults.toConfiguration()
         gradleConfig.apply(eclipseConfig)
 
         then:
         gradleConfig.hasSameUniqueAttributes(eclipseConfig)
+    }
+
+    static class Attributes implements Cloneable {
+        def tasks
+        def workingDir
+        def gradleDistr
+        def gradleHome
+        def javaHome
+        def arguments
+        def jvmArguments
+        def showExecutionView
+        def showConsoleView
+
+        def GradleRunConfigurationAttributes toConfiguration() {
+            GradleRunConfigurationAttributes.with(tasks, workingDir, gradleDistr, gradleHome, javaHome, jvmArguments, arguments, showExecutionView, showConsoleView)
+        }
+
+        def Attributes copy(@DelegatesTo(value = Attributes, strategy=Closure.DELEGATE_FIRST) Closure closure) {
+            def clone = clone()
+            def Closure clonedClosure =  closure.clone()
+            clonedClosure.setResolveStrategy(Closure.DELEGATE_FIRST)
+            clonedClosure.setDelegate(clone)
+            clonedClosure.call(clone)
+            return clone
+        }
+
+        def int size() {
+            Attributes.declaredFields.findAll { it.synthetic == false }.size
+        }
     }
 
 }
