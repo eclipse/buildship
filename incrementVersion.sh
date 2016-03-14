15567994c7eeb53fb821e3e2140c64c4d104f306
Stefan Oehme <st.oehme@gmail.com>
2016-03-14 09:45:04 -0400

    Revert back to 1.0.11
    The increment script was broken and I forgot to rever before running it again.

--- incrementVersion.sh
+++ incrementVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#export GIT_SSH="ssh -i ~/.ssh/id_rsa_gradlewaregitbot_buildship"
+export GIT_SSH="ssh -i ~/.ssh/id_rsa_gradlewaregitbot_buildship"
 git commit -a -m"Increment version from $1 to $2"
 git push origin master
