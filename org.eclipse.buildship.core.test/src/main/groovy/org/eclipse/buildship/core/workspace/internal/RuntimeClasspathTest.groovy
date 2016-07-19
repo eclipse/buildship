6371cf0da2ebd816bd42dcd4228eda9441f453bd
Donat Csikos <csdonat@gmail.com>
2016-07-19 10:10:55 +0200

    Add test coverage to runtime entry resolver

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/RuntimeClasspathTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/RuntimeClasspathTest.groovy
@@ -2,29 +2,21 @@ package org.eclipse.buildship.core.workspace.internal
 
 import org.eclipse.jdt.core.IJavaProject
 import org.eclipse.jdt.core.JavaCore
+import org.eclipse.jdt.launching.IRuntimeClasspathEntry
 import org.eclipse.jdt.launching.JavaRuntime
 
 import org.eclipse.buildship.core.test.fixtures.ProjectSynchronizationSpecification
 
 class RuntimeClasspathTest extends ProjectSynchronizationSpecification {
 
-    def "project classpath has entries only from local Gradle classpath container"() {
-        setup:
-        File location = sampleProject()
-        importAndWait(location)
+    File location
+    File buildFile
 
-        when:
-        IJavaProject javaProject = JavaCore.create(findProject('c'))
-        String[] classpath = JavaRuntime.computeDefaultRuntimeClassPath(javaProject)
-
-        then:
-        classpath.find { it.contains('log4j-1.2.17') }
-        !classpath.find { it.contains('log4j-1.2.16') }
-    }
-
-    private def sampleProject() {
-        dir('sample-project') {
-            file 'build.gradle',  '''
+    void setup() {
+        location = dir('sample-project') {
+            file('settings.gradle') << "include 'a', 'b', 'c'"
+            dir('a'); dir('b'); dir('c')
+            buildFile = file 'build.gradle', '''
                 subprojects {
                     apply plugin: 'java'
 
@@ -32,38 +24,102 @@ class RuntimeClasspathTest extends ProjectSynchronizationSpecification {
                         mavenCentral()
                     }
                 }
-
-                project(':a') {
-                    dependencies {
-                        compile 'log4j:log4j:1.2.17'
-                    }
-                }
-
-                project(':b') {
-                    dependencies {
-                        compile 'log4j:log4j:1.2.16'
-                    }
-                }
-
-                project(':c') {
-                    dependencies {
-                        compile project(':a')
-                        compile project(':b')
-                        testCompile 'junit:junit:4.12'
-                    }
-                }
             '''
-
-            file('settings.gradle') << '''
-                rootProject.name= 'sample-project'
-                include "a"
-                include "b"
-                include "c"
-            '''
-
-            dir('a')
-            dir('b')
-            dir('c')
         }
+    }
+
+    def "Project classpath has entries only from local Gradle classpath container"() {
+        setup:
+        buildFile << '''
+            project(':a') {
+                dependencies {
+                    compile 'log4j:log4j:1.2.17'
+                }
+            }
+
+            project(':b') {
+                dependencies {
+                    compile 'log4j:log4j:1.2.16'
+                }
+            }
+
+            project(':c') {
+                dependencies {
+                    compile project(':a')
+                    compile project(':b')
+                    testCompile 'junit:junit:4.12'
+                }
+            }
+        '''
+        importAndWait(location)
+
+        when:
+        IJavaProject javaProject = JavaCore.create(findProject('c'))
+        IRuntimeClasspathEntry[] classpath = projectRuntimeClasspath(javaProject)
+
+        then:
+        classpath.find { it.path.lastSegment() == 'log4j-1.2.17.jar' }
+        !classpath.find { it.path.lastSegment() == 'log4j-1.2.16.jar' }
+    }
+
+    def "Dependencies of project dependencies are removed from the runtime classpath"() {
+        setup:
+        buildFile << '''
+            project(':a') {
+                dependencies {
+                    compile 'org.springframework:spring-core:4.3.1.RELEASE'
+                }
+            }
+
+            project(':b') {
+                configurations.all {
+                    exclude group: 'commons-logging'
+                }
+
+                dependencies {
+                    compile project(':a')
+                    compile 'org.springframework:spring-core:4.3.1.RELEASE'
+                }
+            }
+        '''
+        importAndWait(location)
+
+        when:
+        IJavaProject javaProject = JavaCore.create(findProject('b'))
+        IRuntimeClasspathEntry[] classpath = projectRuntimeClasspath(javaProject)
+
+        then:
+        !classpath.find { it.path.toPortableString().contains('commons-logging') }
+    }
+
+    def "Dependencies are still on the runtime classpath"() {
+        setup:
+        new File(location, 'b/lib').mkdirs()
+        buildFile << '''
+            project(':b') {
+                dependencies {
+                    compile project(':a')
+                    compile 'log4j:log4j:1.2.17'
+                    compile files('lib')
+                }
+            }
+        '''
+        importAndWait(location)
+
+        when:
+        IJavaProject javaProject = JavaCore.create(findProject('b'))
+        IRuntimeClasspathEntry[] classpath = projectRuntimeClasspath(javaProject)
+
+        then:
+        classpath.length == 3
+        classpath.find { it.type == IRuntimeClasspathEntry.PROJECT && it.path.lastSegment() == 'a' }
+        classpath.find { it.type == IRuntimeClasspathEntry.ARCHIVE && it.path.lastSegment() == 'log4j-1.2.17.jar' }
+        classpath.find { it.type == IRuntimeClasspathEntry.ARCHIVE && it.path.lastSegment() == 'lib' }
+
+    }
+
+    private IRuntimeClasspathEntry[] projectRuntimeClasspath(IJavaProject project) {
+        IRuntimeClasspathEntry projectEntry = JavaRuntime.computeUnresolvedRuntimeClasspath(project).find { it.path == project.project.fullPath }
+        JavaRuntime.resolveRuntimeClasspathEntry(projectEntry, project)
     }
 }
