51d897f5a63a365e51102c1fca14181b66aa31b8
Donat Csikos <csdonat@gmail.com>
2016-11-03 20:04:56 +0100

    Use 'rerun task' icon that is consistent with the platform
    We use the same icon as it is in the JUnit view.

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/PluginImages.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/PluginImages.java
@@ -44,7 +44,7 @@ public enum PluginImages implements PluginImage {
     REMOVE_CONSOLE(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/remove_page.png", ImageState.DISABLED, "icons/full/dlcl16/remove_page.png")),
     REMOVE_ALL_CONSOLES(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/remove_all_pages.png", ImageState.DISABLED, "icons/full/dlcl16/remove_all_pages.png")),
     CANCEL_BUILD_EXECUTION(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/cancel_build_execution.png", ImageState.DISABLED, "icons/full/dlcl16/cancel_build_execution.png")),
-    RERUN_BUILD(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/run.png", ImageState.DISABLED, "icons/full/dlcl16/run.png")),
+    RERUN_BUILD(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/rerun.png", ImageState.DISABLED, "icons/full/dlcl16/rerun.png")),
     RERUN_FAILED_TESTS(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/rerun_failed_tests.png", ImageState.DISABLED, "icons/full/dlcl16/rerun_failed_tests.png")),
     EXPAND_NODE(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/expand_node.png")),
     COLLAPSE_NODE(ImmutableMap.of(ImageState.ENABLED, "icons/full/elcl16/collapse_node.png")),
