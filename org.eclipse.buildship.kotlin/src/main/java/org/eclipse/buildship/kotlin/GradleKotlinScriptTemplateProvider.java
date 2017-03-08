cb3ecc20df79b6c38b6ca11e6fc376b918d996b8
Donat Csikos <csdonat@gmail.com>
2017-03-08 11:35:33 +0100

    Calculate Kotlin script classpath from BuildEnvironment and remove GSK jar from classpath

--- org.eclipse.buildship.kotlin/src/main/java/org/eclipse/buildship/kotlin/GradleKotlinScriptTemplateProvider.java
+++ org.eclipse.buildship.kotlin/src/main/java/org/eclipse/buildship/kotlin/GradleKotlinScriptTemplateProvider.java
@@ -9,147 +9,208 @@
 package org.eclipse.buildship.kotlin;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.gradle.script.lang.kotlin.support.KotlinBuildScriptModel;
+import org.eclipse.buildship.core.CorePlugin;
+import org.eclipse.buildship.core.GradlePluginsRuntimeException;
+import org.eclipse.buildship.core.configuration.GradleProjectNature;
+import org.eclipse.buildship.core.configuration.ProjectConfiguration.ConversionStrategy;
+import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.model.build.BuildEnvironment;
+import org.gradle.tooling.model.build.GradleEnvironment;
 import org.jetbrains.kotlin.core.model.ScriptTemplateProviderEx;
 
 import com.google.common.collect.Lists;
-
-import com.gradleware.tooling.toolingclient.GradleDistribution;
+import com.google.common.io.Files;
 import com.gradleware.tooling.toolingmodel.repository.FixedRequestAttributes;
 
-import org.eclipse.core.resources.IFile;
-
-import org.eclipse.buildship.core.CorePlugin;
-import org.eclipse.buildship.core.GradlePluginsRuntimeException;
-import org.eclipse.buildship.core.configuration.ProjectConfiguration.ConversionStrategy;
-import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper;
-
 /**
- * Contributes the Gradle Kotlin Script template to the Kotlin Eclipse integration.
+ * Contributes the Gradle Kotlin Script template to the Kotlin Eclipse
+ * integration.
  *
  * @author Donat Csikos
  */
 public class GradleKotlinScriptTemplateProvider implements ScriptTemplateProviderEx {
 
-    // properties names defined in gradle-script-kotlin
-    private static final String GSK_PROJECT_ROOT = "projectRoot";
-    private static final String GSK_GRADLE_USER_HOME = "gradleUserHome";
-    private static final String GSK_JAVA_HOME = "gradleJavaHome";
-    private static final String GSK_OPTIONS = "gradleOptions";
-    private static final String GSK_JVM_OPTIONS = "gradleJvmOptions";
-    private static final String GSK_INSTALLATION_LOCAL = "gradleHome";
-    private static final String GSK_INSTALLATION_REMOTE = "gradleUri";
-    private static final String GSK_INSTALLATION_VERSION = "gradleVersion";
+	// properties names defined in gradle-script-kotlin
+	private static final String GSK_PROJECT_ROOT = "projectRoot";
+	private static final String GSK_GRADLE_USER_HOME = "gradleUserHome";
+	private static final String GSK_JAVA_HOME = "gradleJavaHome";
+	private static final String GSK_OPTIONS = "gradleOptions";
+	private static final String GSK_JVM_OPTIONS = "gradleJvmOptions";
+	private static final String GSK_INSTALLATION_LOCAL = "gradleHome";
+	private static final String GSK_INSTALLATION_REMOTE = "gradleUri";
+	private static final String GSK_INSTALLATION_VERSION = "gradleVersion";
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public Iterable<String> getTemplateClasspath(Map<String, ? extends Object> environment) {
-        File projectDir = (File) environment.get(GSK_PROJECT_ROOT);
-        File gradleUserHome = (File) environment.get(GSK_GRADLE_USER_HOME);
-        File javaHome = (File)environment.get(GSK_JAVA_HOME);
-        List<String> arguments = (List<String>) environment.get(GSK_OPTIONS);
-        List<String> jvmArguments = (List<String>) environment.get(GSK_JVM_OPTIONS);
+	@Override
+	public boolean isApplicable(IFile file) {
+		IProject project = file.getProject();
+		return GradleProjectNature.isPresentOn(project);
+	}
 
-        GradleDistribution gradleDistribution = gradleDistributionFor(environment);
+	@Override
+	public Iterable<String> getTemplateClasspath(Map<String, ? extends Object> environment, IProgressMonitor monitor) {
+		BuildEnvironment buildEnvironment = queryBuildEnvironment(environment);
+		GradleEnvironment gradleEnvironment = buildEnvironment.getGradle();
 
-        List<String> effectiveJvmArguments = Lists.newArrayList("-Dorg.gradle.script.lang.kotlin.provider.mode=classpath"); // from KotlinScriptPluginFactory
-        effectiveJvmArguments.addAll(jvmArguments);
+		File gradleUserHome = gradleEnvironment.getGradleUserHome();
+		String gradleVersion = gradleEnvironment.getGradleVersion();
+		File distroRoot = findDistributionRoot(gradleUserHome, gradleVersion);
 
-        KotlinBuildScriptModel model = queryTemplateClasspath(projectDir, gradleDistribution, gradleUserHome, javaHome, arguments, effectiveJvmArguments);
+		if (distroRoot == null) {
+			return Collections.emptyList();
+		}
 
-        List<String> result = Lists.newArrayList();
-        if (model != null) {
-            for (File f : model.getClassPath()) {
-                result.add(f.getAbsolutePath());
-            }
-        }
-        return result;
-    }
+		List<String> result = Lists.newArrayList();
+		addGradleScriptKotlinJar(distroRoot, result);
+		addGeneratedGradleJars(gradleUserHome, gradleVersion, result);
 
-    private static GradleDistribution gradleDistributionFor(Map<String, ? extends Object> environment) {
-        File gradleLocal = (File) environment.get(GSK_INSTALLATION_LOCAL);
-        URI gradleRemote = (URI) environment.get(GSK_INSTALLATION_REMOTE);
-        String gradleVersion = (String) environment.get(GSK_INSTALLATION_VERSION);
-        if (gradleLocal != null) {
-            return GradleDistribution.forLocalInstallation(gradleLocal);
-        } else if (gradleRemote != null) {
-            return GradleDistribution.forRemoteDistribution(gradleRemote);
-        } else if (gradleVersion != null) {
-            return GradleDistribution.forVersion(gradleVersion);
-        } else {
-            return GradleDistribution.fromBuild();
-        }
-    }
+		// TODO (donat) add the ${gradleUserHome}/caches/3.5-20170305000422+0000/gradle-script-kotlin/${scriptMd5Hash} to the classpath
+		// The classes defining the accessors (`application { ... }` and friends) are compiled dynamically to that folder
+		// The related MD5 calculation logic in GSK: https://github.com/gradle/gradle-script-kotlin/blob/98ec7c0c42f740ef14c21aa39a2bc9ae98f96397/src/main/kotlin/org/gradle/script/lang/kotlin/resolver/KotlinBuildScriptDependenciesResolver.kt#L72-L92
+		return result;
+	}
 
-    private static KotlinBuildScriptModel queryTemplateClasspath(File projectDir, GradleDistribution gradleDistribution, File gradleUserHome, File javaHome, List<String> arguments, List<String> jvmArguments) {
-        ProjectConnection connection = null;
-        try {
-            GradleConnector connector = GradleConnector.newConnector().forProjectDirectory(projectDir);
-            connector.useGradleUserHomeDir(gradleUserHome);
-            gradleDistribution.apply(connector);
-            connection = connector.connect();
-            return connection.model(KotlinBuildScriptModel.class)
-                .setJvmArguments(jvmArguments)
-                .withArguments(arguments)
-                .setJavaHome(javaHome)
-                .get();
-        } catch (Exception e) {
-            CorePlugin.logger().warn("Cannot query Kotlin build script classpath model", e);
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
-        }
-    }
+	@SuppressWarnings("unchecked")
+	private static BuildEnvironment queryBuildEnvironment(Map<String, ? extends Object> environment) {
+		List<String> jvmArguments = Lists.newArrayList((List<String>) environment.get(GSK_JVM_OPTIONS));
+		jvmArguments.add("-Dorg.slf4j.simpleLogger.defaultLogLevel=debug");
+		List<String> effectiveJvmArguments = Lists.newArrayList("-Dorg.gradle.script.lang.kotlin.provider.mode=classpath"); // from  KotlinScriptPluginFactory
+		effectiveJvmArguments.addAll(jvmArguments);
 
-    @Override
-    public Map<String, Object> getEnvironment(IFile file) {
-        HashMap<String, Object> environment = new HashMap<String, Object>();
-        FixedRequestAttributes attributes = CorePlugin.projectConfigurationManager().readProjectConfiguration(file.getProject()).toRequestAttributes(ConversionStrategy.MERGE_WORKSPACE_SETTINGS);
+		ProjectConnection connection = null;
+		try {
+			GradleConnector connector = GradleConnector.newConnector().forProjectDirectory((File) environment.get(GSK_PROJECT_ROOT));
+			connector.useGradleUserHomeDir((File) environment.get(GSK_GRADLE_USER_HOME));
+			applyGradleDistribution(environment, connector);
+			connection = connector.connect();
+			return connection.model(BuildEnvironment.class)
+					.setJvmArguments(effectiveJvmArguments)
+					.withArguments((List<String>) environment.get(GSK_OPTIONS))
+					.setJavaHome((File) environment.get(GSK_JAVA_HOME))
+					.get();
+		} catch (Exception e) {
+			CorePlugin.logger().warn("Cannot query BuildEnvironment", e);
+			return null;
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
 
-        environment.put(GSK_PROJECT_ROOT, attributes.getProjectDir());
-        environment.put(GSK_GRADLE_USER_HOME, attributes.getGradleUserHome());
-        environment.put(GSK_JAVA_HOME, attributes.getJavaHome());
-        environment.put(GSK_OPTIONS, attributes.getArguments());
-        environment.put(GSK_JVM_OPTIONS, attributes.getJvmArguments());
+	private static void applyGradleDistribution(Map<String, ? extends Object> environment, GradleConnector connector) {
+		File gradleLocal = (File) environment.get(GSK_INSTALLATION_LOCAL);
+		URI gradleRemote = (URI) environment.get(GSK_INSTALLATION_REMOTE);
+		String gradleVersion = (String) environment.get(GSK_INSTALLATION_VERSION);
+		if (gradleLocal != null) {
+			connector.useInstallation(gradleLocal);
+		} else if (gradleRemote != null) {
+			connector.useDistribution(gradleRemote);
+		} else if (gradleVersion != null) {
+			connector.useGradleVersion(gradleVersion);
+		} else {
+			connector.useBuildDistribution();
+		}
+	}
 
-        GradleDistributionWrapper gradleDistribution = GradleDistributionWrapper.from(attributes.getGradleDistribution());
-        switch (gradleDistribution.getType()) {
-            case LOCAL_INSTALLATION:
-                environment.put(GSK_INSTALLATION_LOCAL, new File(gradleDistribution.getConfiguration()));
-                break;
-            case REMOTE_DISTRIBUTION:
-                environment.put(GSK_INSTALLATION_REMOTE, createURI(gradleDistribution.getConfiguration()));
-                break;
-            case VERSION:
-                environment.put(GSK_INSTALLATION_VERSION, gradleDistribution.getConfiguration());
-                break;
-            default:
-                break;
-        }
+	private static File findDistributionRoot(File gradleUserHome, final String version) {
+		File distsDir = new File(gradleUserHome, "wrapper/dists");
+		List<File> candidates = Arrays.asList(distsDir.listFiles(new FileFilter() {
 
-        return environment;
-    }
+			@Override
+			public boolean accept(File path) {
+				String name = path.getName();
+				return name.contains("gradle") && name.contains(version);
+			}
+		}));
 
-    @Override
-    public String getTemplateClassName() {
-        return "org.gradle.script.lang.kotlin.KotlinBuildScript";
-    }
+		if (candidates.isEmpty()) {
+			return null;
+		} else {
+			return candidates.get(candidates.size() - 1);
+		}
+	}
 
-    private URI createURI(String uri) {
-        try {
-            return new URI(uri);
-        } catch (URISyntaxException e) {
-            throw new GradlePluginsRuntimeException(e);
-        }
-    }
+	private static void addGradleScriptKotlinJar(File distroRoot, List<String> result) {
+		for (File f1 : Files.fileTreeTraverser().breadthFirstTraversal(distroRoot)) {
+			if (f1.isDirectory() && f1.getName().equals("lib")) {
+				for (File f2 : f1.listFiles()) {
+					if (f2.getName().endsWith(".jar") && f2.getName().startsWith("gradle-script-kotlin")) {
+						result.add(f2.getAbsolutePath());
+						return;
+					}
+				}
+			}
+		}
+		CorePlugin.logger().warn("Can't find gradle-script-kotlin jar in " + distroRoot.getAbsolutePath());
+	}
+
+	private static void addGeneratedGradleJars(File gradleUserHome, String gradleVersion, List<String> result) {
+		File cacheDir = new File(gradleUserHome, "caches/" + gradleVersion);
+		File generatedJarsDir = new File(cacheDir, "generated-gradle-jars");
+		if (generatedJarsDir.exists()) {
+			for (File f : Files.fileTreeTraverser().breadthFirstTraversal(generatedJarsDir)) {
+				if (f.getName().endsWith("jar")) {
+					result.add(f.getAbsolutePath());
+				}
+			}
+		}
+	}
+
+	@Override
+	public Map<String, Object> getEnvironment(IFile file) {
+		HashMap<String, Object> environment = new HashMap<String, Object>();
+		FixedRequestAttributes attributes = CorePlugin.projectConfigurationManager()
+				.readProjectConfiguration(file.getProject())
+				.toRequestAttributes(ConversionStrategy.MERGE_WORKSPACE_SETTINGS);
+
+		environment.put(GSK_PROJECT_ROOT, attributes.getProjectDir());
+		environment.put(GSK_GRADLE_USER_HOME, attributes.getGradleUserHome());
+		environment.put(GSK_JAVA_HOME, attributes.getJavaHome());
+		environment.put(GSK_OPTIONS, attributes.getArguments());
+		environment.put(GSK_JVM_OPTIONS, attributes.getJvmArguments());
+
+		GradleDistributionWrapper gradleDistribution = GradleDistributionWrapper.from(attributes.getGradleDistribution());
+		switch (gradleDistribution.getType()) {
+		case LOCAL_INSTALLATION:
+			environment.put(GSK_INSTALLATION_LOCAL, new File(gradleDistribution.getConfiguration()));
+			break;
+		case REMOTE_DISTRIBUTION:
+			environment.put(GSK_INSTALLATION_REMOTE, createURI(gradleDistribution.getConfiguration()));
+			break;
+		case VERSION:
+			environment.put(GSK_INSTALLATION_VERSION, gradleDistribution.getConfiguration());
+			break;
+		default:
+			break;
+		}
+
+		return environment;
+	}
+
+	@Override
+	public String getTemplateClassName() {
+		return "org.gradle.script.lang.kotlin.KotlinBuildScript";
+	}
+
+	private URI createURI(String uri) {
+		try {
+			return new URI(uri);
+		} catch (URISyntaxException e) {
+			throw new GradlePluginsRuntimeException(e);
+		}
+	}
 }
