7d2ca8719203bf9d874d364ac96cf7f105ef0038
Stefan Oehme <st.oehme@gmail.com>
2017-07-05 18:31:34 +0200

    Move UI aspects out of execution progress listener
    The presentation-related code and state is now handled in the 
    execution page. The listener's only job is to forward events to
    a job that buffers events for asynchronous UI updates. Encapsulating
    the state in the UI element means that we no longer need to worry
    about concurrency, except for using a thread-safe queue for buffering
    the events. Everything else happens in the UI thread.

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/view/execution/UpdateExecutionPageJob.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/view/execution/UpdateExecutionPageJob.java
@@ -11,23 +11,18 @@
 
 package org.eclipse.buildship.ui.view.execution;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-import org.gradle.tooling.events.FailureResult;
-import org.gradle.tooling.events.FinishEvent;
-import org.gradle.tooling.events.task.TaskOperationDescriptor;
+import org.gradle.tooling.events.ProgressEvent;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.MapMaker;
+import com.google.common.collect.Lists;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
@@ -39,49 +34,32 @@ public final class UpdateExecutionPageJob extends Job {
 
     private static final int REPEAT_DELAY = 100;
 
-    private final TreeViewer treeViewer;
-    // if the value is true then the operation can be removed
-    private final ConcurrentMap<OperationItem, Boolean> activeItems;
+    private final ExecutionPage page;
+    private final BlockingQueue<ProgressEvent> queue = new LinkedBlockingQueue<>();
     private volatile boolean running;
 
-    public UpdateExecutionPageJob(TreeViewer treeViewer) {
+    public UpdateExecutionPageJob(ExecutionPage page) {
         super("Updating duration of non-finished operations");
-        this.treeViewer = treeViewer;
-        this.activeItems = new MapMaker().concurrencyLevel(2).makeMap();
+        this.page = page;
         this.running = true;
+    }
+
+    public void addEvent(ProgressEvent event) {
+        this.queue.add(event);
     }
 
     @Override
     protected IStatus run(IProgressMonitor monitor) {
-        // use a copy of to ignore concurrent changes
-        Map<OperationItem, Boolean> activeItemsCopy = ImmutableMap.copyOf(this.activeItems);
+        List<ProgressEvent> events = Lists.newArrayList();
+        this.queue.drainTo(events);
 
-        // update operations in the view
         Display display = PlatformUI.getWorkbench().getDisplay();
         if (!display.isDisposed()) {
-            display.syncExec(new UpdateExecutionPageContent(this.treeViewer, activeItemsCopy.keySet()));
+            display.syncExec(new UpdateExecutionPageContent(this.page, events));
         }
 
-        // delete updated build operations that were marked removable
-        for (OperationItem item : activeItemsCopy.keySet()) {
-            if (activeItemsCopy.get(item)) {
-                this.activeItems.remove(item);
-            }
-        }
-
-        // reschedule the job such that is runs again in repeatDelay ms
         schedule(REPEAT_DELAY);
         return Status.OK_STATUS;
-    }
-
-    public void startOperationItem(OperationItem item) {
-        Preconditions.checkNotNull(item.getStartEvent());
-        this.activeItems.put(item, Boolean.FALSE);
-    }
-
-    public void stopOperationItem(OperationItem item) {
-        Preconditions.checkNotNull(item.getStartEvent());
-        this.activeItems.replace(item, Boolean.TRUE);
     }
 
     @Override
@@ -97,56 +75,22 @@ public final class UpdateExecutionPageJob extends Job {
      * UI job to refresh active items in the viewer.
      */
     private static class UpdateExecutionPageContent implements Runnable {
+        private final ExecutionPage page;
+        private final List<ProgressEvent> events;
 
-        private final Set<OperationItem> activeItems;
-        private final TreeViewer treeViewer;
-
-        public UpdateExecutionPageContent(TreeViewer treeViewer, Set<OperationItem> activeItems) {
-            this.treeViewer = treeViewer;
-            this.activeItems = activeItems;
+        public UpdateExecutionPageContent(ExecutionPage page, List<ProgressEvent> events) {
+            this.page = page;
+            this.events = events;
         }
 
         @Override
         public void run() {
-            if (!this.treeViewer.getControl().isDisposed()) {
-                for (OperationItem item : this.activeItems) {
-                    this.treeViewer.update(item, null);
+            if (!this.page.getPageControl().isDisposed()) {
+                for(ProgressEvent event : this.events) {
+                    this.page.onProgress(event);
                 }
-
-                this.treeViewer.refresh(false);
-
-                for (OperationItem item : this.activeItems) {
-                    if (shouldBeVisible(item)) {
-                        this.treeViewer.expandToLevel(item, 0);
-                    }
-                }
+                this.page.refreshChangedItems();
             }
-        }
-
-        private boolean shouldBeVisible(OperationItem item) {
-            return isOnMax2ndLevel(item) || isTaskOperation(item) || isFailedOperation(item);
-        }
-
-        private boolean isOnMax2ndLevel(OperationItem item) {
-            int level = 2;
-            while (level >= 0) {
-                if (item.getParent() == null) {
-                    return true;
-                } else {
-                    level--;
-                    item = item.getParent();
-                }
-            }
-            return false;
-        }
-
-        private boolean isTaskOperation(OperationItem item) {
-            return item.getStartEvent().getDescriptor() instanceof TaskOperationDescriptor;
-        }
-
-        private boolean isFailedOperation(OperationItem item) {
-                FinishEvent finishEvent = item.getFinishEvent();
-                return finishEvent != null ? finishEvent.getResult() instanceof FailureResult : false;
         }
     }
 }
