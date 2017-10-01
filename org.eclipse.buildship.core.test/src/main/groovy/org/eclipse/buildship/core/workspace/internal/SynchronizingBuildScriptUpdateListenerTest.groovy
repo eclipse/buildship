3075e6f02ff01b20914bcfc1413e8a16de7ee164
Donat Csikos <csdonat@gmail.com>
2017-10-01 12:40:18 +0200

    Define required test coverage

---
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/SynchronizingBuildScriptUpdateListenerTest.groovy
@@ -0,0 +1,31 @@
+package org.eclipse.buildship.core.workspace.internal
+
+import org.eclipse.buildship.core.test.fixtures.ProjectSynchronizationSpecification
+import org.eclipse.buildship.core.test.fixtures.WorkspaceSpecification
+
+class SynchronizingBuildScriptUpdateListenerTest extends ProjectSynchronizationSpecification {
+
+    def "Execute project synchronization when build.gradle file created"() {
+        // TODO (donat) implement test case
+    }
+
+    def "Execute project synchronization when build.gradle file changes"() {
+        // TODO (donat) implement test case
+    }
+
+    def "Execute project synchronization when build.gradle file deleted"() {
+        // TODO (donat) implement test case
+    }
+
+    def "Execute project synchronization when custom build script changes"() {
+        // TODO (donat) implement test case
+    }
+
+    def "Synchronization can be disabled for the entire workspace"() {
+        // TODO (donat) implement test case
+    }
+
+    def "Synchronization can be disabled for a project"() {
+        // TODO (donat) implement test case
+    }
+}
