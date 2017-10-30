7606c38acef729acc0a092de8249137d2ed8c4b8
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2017-10-30 15:31:53 -0500

    working on tests

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/SynchronizingBuildScriptUpdateListenerTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/workspace/internal/SynchronizingBuildScriptUpdateListenerTest.groovy
@@ -1,12 +1,51 @@
 package org.eclipse.buildship.core.workspace.internal
 
+import org.eclipse.buildship.core.configuration.BuildConfiguration
+import org.eclipse.buildship.core.configuration.WorkspaceConfiguration
 import org.eclipse.buildship.core.test.fixtures.ProjectSynchronizationSpecification
 import org.eclipse.buildship.core.test.fixtures.WorkspaceSpecification
 
+import java.io.File
+
+import org.eclipse.core.resources.IProject
+import org.eclipse.core.runtime.NullProgressMonitor
+import org.eclipse.jdt.core.IClasspathEntry
+import org.eclipse.jdt.core.IJavaProject
+
 class SynchronizingBuildScriptUpdateListenerTest extends ProjectSynchronizationSpecification {
 
-    def "Execute project synchronization when build.gradle file created"() {
-        // TODO (donat) implement test case
+
+
+    /*def "Execute project synchronization when build.gradle file created"() {
+
+        setup: "Create a new Java project for testing"
+
+        IJavaProject javaProject = newJavaProject('sample-project')
+        IClasspathEntry[] entries = javaProject.rawClasspath + JavaCore.newContainerEntry(GradleClasspathContainer.CONTAINER_PATH)
+        javaProject.setRawClasspath(entries, null)
+        BuildConfiguration buildConfig = createOverridingBuildConfiguration(dir('sample-project'), DEFAULT_DISTRIBUTION)
+
+        expect:
+        !javaProject.getResolvedClasspath(false).find{ it.path.toPortableString().endsWith('spring-beans-1.2.8.jar') }
+
+        when:
+        getConfigurationManager()
+            .saveWorkspaceConfiguration(configurationManager
+                .saveWorkspaceConfiguration(
+                    new WorkspaceConfiguration(GradleDistribution.fromBuild(), null, false, false, true)))
+
+        def projectDir = dir('sample-project') {
+            file 'build.gradle','''apply plugin: "java"
+               repositories { jcenter() }
+               dependencies { compile "org.springframework:spring-beans:1.2.8"}
+            '''
+            dir 'src/main/java'
+        }
+
+        waitForGradleJobsToFinish()
+
+        then:
+        javaProject.getResolvedClasspath(false).find{ it.path.toPortableString().endsWith('spring-beans-1.2.8.jar') }
     }
 
     def "Execute project synchronization when build.gradle file changes"() {
@@ -27,5 +66,5 @@ class SynchronizingBuildScriptUpdateListenerTest extends ProjectSynchronizationS
 
     def "Synchronization can be disabled for a project"() {
         // TODO (donat) implement test case
-    }
+    }*/
 }
