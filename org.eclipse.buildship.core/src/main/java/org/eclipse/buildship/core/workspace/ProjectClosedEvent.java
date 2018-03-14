0d3e3f3696276b9272a23c2abba05afa942fd334
Prashanth Rao <iamprashanthrao@gmail.com>
2018-03-14 20:40:17 +0100

    Updated author name

--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/ProjectClosedEvent.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/workspace/ProjectClosedEvent.java
@@ -13,7 +13,7 @@ import org.eclipse.core.resources.IProject;
 /**
  * Project closed event.
  *
- * @author Prashanth Parameshwara
+ * @author Prashanth Rao
  */
 public class ProjectClosedEvent extends BaseProjectChangedEvent {
 
