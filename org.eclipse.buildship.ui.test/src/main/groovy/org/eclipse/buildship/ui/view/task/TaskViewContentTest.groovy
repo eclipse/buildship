c7f47f2a6d75650f9e44be849ab59d9aa2c2646a
Prashanth Rao <iamprashanthrao@gmail.com>
2018-04-13 14:35:55 +0530

    Supported synchronization between Task View and Project Explorer on Close/open operation on project.
    Signed-off-by: Prashanth Rao <iamprashanthrao@gmail.com>

--- org.eclipse.buildship.ui.test/src/main/groovy/org/eclipse/buildship/ui/view/task/TaskViewContentTest.groovy
+++ org.eclipse.buildship.ui.test/src/main/groovy/org/eclipse/buildship/ui/view/task/TaskViewContentTest.groovy
@@ -170,6 +170,24 @@ class TaskViewContentTest extends BaseTaskViewTest {
         taskTree.find { it == 'a' }
         !taskTree.find { it == 'b' }
     }
+    
+    def "The task view is refreshed when projects are closed/opened"() {
+        given:
+        def closed = dir("a") { file 'build.gradle' }
+        def opened = dir("b") { file 'build.gradle' }
+        importAndWait(closed)
+        importAndWait(opened)
+        fileTree(closed) { file 'build.gradle', "error" }
+
+        when:
+        findProject("b").close(null)
+        waitForTaskView()
+
+        then:
+        taskTree.find { it == 'a' }
+        !taskTree.find { it == 'b' }
+    }
+    
 
     def "Subprojects should be under the parent project's folder when not showing flatten project hierarchy"() {
         given:
