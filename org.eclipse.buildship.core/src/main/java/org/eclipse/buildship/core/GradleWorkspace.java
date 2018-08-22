7a44fa8ebab1e6b19cd7e63b54624dbb1134e95e
Donát Csikós <csdonat@gmail.com>
2018-08-22 18:17:59 +0200

    Introduce GradleCore and GradleWorkspace interface

---
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/GradleWorkspace.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 the original author or authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.buildship.core;
+
+/**
+ * Manages the Gradle builds contained in the current Eclipse workspace.
+ *
+ * @author Donat Csikos
+ * @since 3.0
+ */
+public interface GradleWorkspace {
+
+}
---
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/internal/DefaultGradleWorkspace.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 the original author or authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.buildship.core.internal;
+
+import org.eclipse.buildship.core.GradleWorkspace;
+
+/**
+ * Default implementation for {@link GradleWorkspace}.
+ *
+ * @author Donat Csikos
+ */
+public final class DefaultGradleWorkspace implements GradleWorkspace {
+
+}
