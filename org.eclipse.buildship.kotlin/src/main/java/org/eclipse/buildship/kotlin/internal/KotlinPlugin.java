5c6c20785ce04aa2f61852ac6a490eeb1324d035
Donát Csikós <csdonat@gmail.com>
2018-08-22 12:00:01 +0200

    Adjust kotlin plugin

--- org.eclipse.buildship.kotlin/src/main/java/org/eclipse/buildship/kotlin/internal/KotlinPlugin.java
+++ org.eclipse.buildship.kotlin/src/main/java/org/eclipse/buildship/kotlin/internal/KotlinPlugin.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.eclipse.buildship.kotlin;
+package org.eclipse.buildship.kotlin.internal;
 
 import org.osgi.framework.BundleContext;
 
