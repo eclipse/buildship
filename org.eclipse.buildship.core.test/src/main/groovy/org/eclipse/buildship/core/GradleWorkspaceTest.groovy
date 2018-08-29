eff9dc552aa92a964fc13ccc76f3bdad47ac5b77
Donát Csikós <csdonat@gmail.com>
2018-08-29 10:51:20 +0200

    Add initial API to import new Gradle projects

--- org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/GradleWorkspaceTest.groovy
+++ org.eclipse.buildship.core.test/src/main/groovy/org/eclipse/buildship/core/GradleWorkspaceTest.groovy
@@ -3,33 +3,50 @@ package org.eclipse.buildship.core
 import org.eclipse.core.resources.IProject
 import org.eclipse.core.runtime.NullProgressMonitor
 
+import org.eclipse.buildship.core.configuration.BuildConfiguration
 import org.eclipse.buildship.core.internal.CorePlugin
 import org.eclipse.buildship.core.internal.configuration.GradleProjectNature
 import org.eclipse.buildship.core.internal.test.fixtures.WorkspaceSpecification
 
 class GradleWorkspaceTest extends WorkspaceSpecification {
 
-    def "Cannot get reference to a null Gradle build"() {
+    def "Cannot get a null build"() {
         expect:
         !GradleCore.workspace.getBuild(null).present
     }
 
-    def "Cannot get reference to Gradle build if target project is inaccessible"() {
+    def "Cannot get a Gradle build if target project is inaccessible"() {
         expect:
         !GradleCore.workspace.getBuild(newClosedProject("GradleWorkspaceTest")).present
     }
 
-    def "Cannot get reference to Gradle build if target project is not a Gradle project"() {
+    def "Cannot get a Gradle build if target project is not a Gradle project"() {
         expect:
         !GradleCore.workspace.getBuild(newProject("GradleWorkspaceTest")).present
     }
 
-    def "Can get reference to Gradle build via valid Gradle proejct"() {
+    def "Can get a Gradle build via valid Gradle proejct"() {
         setup:
         IProject project = newProject("GradleWorkspaceTest")
         CorePlugin.workspaceOperations().addNature(project, GradleProjectNature.ID, new NullProgressMonitor())
 
         expect:
         GradleCore.workspace.getBuild(project).present
+    }
+
+    def "Cannot create a null build"() {
+        when:
+        GradleCore.workspace.createBuild(null)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+    def "Can create a Gradle build"() {
+        setup:
+        BuildConfiguration configuration = BuildConfiguration.forRootProjectDirectory(dir('GradleWorkspaceTest')).build()
+
+        expect:
+        GradleCore.workspace.createBuild(configuration)
     }
 }
