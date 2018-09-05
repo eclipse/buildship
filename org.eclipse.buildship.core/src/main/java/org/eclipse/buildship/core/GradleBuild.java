0baf394810d2996eaf31ec8b1abb400eab3f09c7
Donát Csikós <csdonat@gmail.com>
2018-09-05 18:04:20 +0200

    Add javadoc to the synchronize() method

--- org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/GradleBuild.java
+++ org.eclipse.buildship.core/src/main/java/org/eclipse/buildship/core/GradleBuild.java
@@ -19,9 +19,35 @@ import org.eclipse.core.runtime.IProgressMonitor;
 public interface GradleBuild {
 
     /**
-     * Synchronizes this build with the workspace.
+     * Synchronizes the workspace with this Gradle build.
      *
-     * @param monitor the progress monitor
+     * <p>
+     * The method loads the Gradle build configuration and updates the workspace based on the
+     * retrieved information.The algorithm is as follows:
+     * <ul>
+     *   <li>Synchronize all Gradle projects of the Gradle build with the Eclipse workspace
+     *       project counterparts. If there is a project in the workspace at the location of the
+     *       Gradle project, the synchronization is as follows:
+     *     <ul>
+     *       <li>If the workspace project is closed, the project is left unchanged.</li>
+     *       <li>If the workspace project is open, the project configuration (name, source directories,
+     *           dependencies, etc.) is updated.</li>
+     *     </ul>
+     *   </li>
+     *   <li>Uncouple all open workspace projects for which there is no corresponding Gradle project
+     *       in the Gradle build anymore. This includes removing the Gradle project natures and the
+     *       corresponding settings file.</li>
+     * </ul>
+     *
+     * <p>
+     * This is a long-running operation which blocks the current thread until completion. Progress
+     *  and cancellation are provided via the monitor.
+     * <p>
+     *
+     * The result of the synchronization - let it be a success or a failure - is described by the returned
+     * {@link SynchronizationResult} instance.
+     *
+     * @param monitor the monitor to report progress on
      * @return the synchronization result
      */
     SynchronizationResult synchronize(IProgressMonitor monitor);
