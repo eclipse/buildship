1ac7f34fc4e3c45b51d3980f4c83d0ab5bb3cf7b
Donat Csikos <csdonat@gmail.com>
2020-05-15 15:49:41 +0200

    Fix build failures

--- org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/internal/wizard/workspacecomposite/AbstractWorkspaceCompositeWizard.java
+++ org.eclipse.buildship.ui/src/main/java/org/eclipse/buildship/ui/internal/wizard/workspacecomposite/AbstractWorkspaceCompositeWizard.java
@@ -1,14 +1,12 @@
-/*
- * Copyright (c) 2015 the original author or authors.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*******************************************************************************
+ * Copyright (c) 2019 Gradle Inc.
  *
- * Contributors:
- *     Simon Scholz <simon.scholz@vogella.com> - Bug 473545
- *     Sebastian Kuzniarz (Diebold Nixdorf Inc.) - adaptation and customization for workspace composite wizard, refactored HelpContextIdProvider
- */
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 
 package org.eclipse.buildship.ui.internal.wizard.workspacecomposite;
 
