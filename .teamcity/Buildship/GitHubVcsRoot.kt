a52a194888e3d4faa4a8de466d426fb05778f0b6
Guy Brand <guy@gradle.com>
2020-07-09 17:18:48 +0200

    Update TeamCity configuration
    - Make TeamCity DSL read only
    - Remove gradlewaregitbot references

--- .teamcity/Buildship/GitHubVcsRoot.kt
+++ .teamcity/Buildship/GitHubVcsRoot.kt
@@ -4,9 +4,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object GitHubVcsRoot : GitVcsRoot({
     name = "Buildship"
-    url = "https://gradlewaregitbot@github.com/eclipse/buildship.git"
+    url = "https://github.com/eclipse/buildship.git"
     branchSpec = "+:refs/heads/*"
-    userForTags = "Gradleware GitHubVcsRoot Bot <gradlewa/**/regitbot@gradleware.com>"
     agentGitPath = "%env.TEAMCITY_GIT_PATH%"
     agentCleanFilesPolicy = GitVcsRoot.AgentCleanFilesPolicy.NON_IGNORED_ONLY
     useMirrors = false
