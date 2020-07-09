27b9331616316b3097fdbed614a6f10a25003d4e
Guy Brand <guy@guymas.ch>
2020-07-09 17:29:22 +0200

    Update TeamCity configuration (#1003)
    - Make TeamCity DSL read only
    - Remove gradlewaregitbot references

--- .teamcity/Buildship/Project.kt
+++ .teamcity/Buildship/Project.kt
@@ -14,6 +14,8 @@ object Project : Project({
         param("env.JAVA_TOOL_OPTIONS", "-Dfile.encoding=UTF8")
         password("eclipse.downloadServer.password", "credentialsJSON:e600b1ef-46bb-4ced-b45b-795da6964956", label = "Password", display = ParameterDisplay.HIDDEN)
         password("eclipse.downloadServer.username", "credentialsJSON:23f3947f-45b2-46b8-83f6-9341c9b914f6", label = "Username", display = ParameterDisplay.HIDDEN)
+        // Do not allow UI changes in the TeamCity configuration
+        param("teamcity.ui.settings.readOnly", "true")
     }
 
     cleanup {
