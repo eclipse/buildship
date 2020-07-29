0f3e0ee5476b9d54d8212ee55d40e8c757d60a39
Vaidotas Valuckas <rieske@users.noreply.github.com>
2020-07-29 17:38:16 +0300

    Add failure condition to detect potential credentials leaks to both build and promotion templates (#1014)
    Uses a fake unmasked credentials value to fail the build. This fake value is added in TC build parameters and environment variables. It won't detect every credentials leak, but will at least fail the build if all the environment variables or build properties (that might include sensitive values) are dumped during the build.
    
    Signed-off-by: Vaidotas Valuckas <vaidotas.valuckas@gmail.com>

---
+++ .teamcity/Buildship/extensions.kt
@@ -0,0 +1,20 @@
+package Buildship
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.Template
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnText
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnText
+import jetbrains.buildServer.configs.kotlin.v2019_2.ui.add
+
+fun Template.addCredentialsLeakFailureCondition() {
+    failureConditions {
+        add {
+            failOnText {
+                conditionType = BuildFailureOnText.ConditionType.CONTAINS
+                pattern = "%unmaskedFakeCredentials%"
+                failureMessage = "This build might be leaking credentials"
+                reverse = false
+                stopBuildOnFailure = true
+            }
+        }
+    }
+}
