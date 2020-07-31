c169e83432da8bc0193d681d98449d035f95a55f
Donat Csikos <csdonat@gmail.com>
2020-07-31 16:08:55 +0200

    Hide build artifacts in TeamCity

--- .teamcity/Buildship/Promotion30/Promotion30Template.kt
+++ .teamcity/Buildship/Promotion30/Promotion30Template.kt
@@ -12,8 +12,6 @@ object Promotion30Template : Template({
 
     artifactRules = "org.eclipse.buildship.site/build/repository/** => .teamcity/update-site"
 
-    addCredentialsLeakFailureCondition()
-
     // The artifact upload requires uses ssh which requires manual confirmation. to work around that, we use the same
     // machine for the upload.
     // TODO We should separate the update site generation and the artifact upload into two separate steps.
