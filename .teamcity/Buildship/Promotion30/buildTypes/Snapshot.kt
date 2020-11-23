d9dbe215f2023d1df9d94123d551abf5e192be3f
Donat Csikos <csdonat@gmail.com>
2020-11-23 11:40:44 +0100

    Update pipeline

--- .teamcity/Buildship/Promotion30/buildTypes/Snapshot.kt
+++ .teamcity/Buildship/Promotion30/buildTypes/Snapshot.kt
@@ -196,6 +196,20 @@ object Snapshot : BuildType({
                 "-Dgradle.cache.remote.password=%gradle.cache.remote.password%"
             """.trimIndent()
         }
+
+        gradle {
+            name = "Build and upload update site for Eclipse 2020-06"
+            tasks = "clean build uploadUpdateSite"
+            buildFile = ""
+            gradleParams = """
+                --exclude-task eclipseTest
+                -Peclipse.version=416 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
+                --stacktrace -Declipse.p2.mirror=false
+                "-Dgradle.cache.remote.url=%gradle.cache.remote.url%"
+                "-Dgradle.cache.remote.username=%gradle.cache.remote.username%"
+                "-Dgradle.cache.remote.password=%gradle.cache.remote.password%"
+            """.trimIndent()
+        }
     }
 
     triggers {
