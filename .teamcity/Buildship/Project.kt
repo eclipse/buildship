9c42f2d62c4fd75020678ea206c3299042b309f1
Donat Csikos <csdonat@gmail.com>
2021-02-12 10:39:19 +0100

    Simplify experimental pipeline

--- .teamcity/Buildship/Project.kt
+++ .teamcity/Buildship/Project.kt
@@ -33,69 +33,57 @@ object Project : Project({
 
 object BuildshipExperimentalPipeline : Project({
     name = "Experimental pipeline"
-    subProject(BuildshipBuilds())
-    subProject(BuildshipStages())
-    subProject(BuildshipTriggers())
+    subProject(BuildshipBuilds)
+    subProject(BuildshipTriggers)
 })
 
-val ib1_1 = IndividualBuildType(type = "SanityCheck", os = "Linux", eclipseVersion = "4.13", javaVersion = "8")
-val ib2_1 = IndividualBuildType(type = "Basic", os = "Linux", eclipseVersion = "4.3", javaVersion = "8")
-val ib2_2 = IndividualBuildType(type = "Basic", os = "Linux", eclipseVersion = "4.12", javaVersion = "8")
-val ib2_3 = IndividualBuildType(type = "Basic", os = "Windows", eclipseVersion = "4.3", javaVersion = "8")
-val ib2_4 = IndividualBuildType(type = "Basic", os = "Windows", eclipseVersion = "4.6", javaVersion = "8")
-val ib3_1 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.4", javaVersion = "8")
-val ib3_2 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.5", javaVersion = "8")
-val ib3_3 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.6", javaVersion = "8")
-val ib3_4 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.7", javaVersion = "8")
-val ib3_5 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.7", javaVersion = "9")
-val ib3_6 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.8", javaVersion = "8")
-val ib3_7 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.9", javaVersion = "8")
-val ib3_8 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.10", javaVersion = "8")
-val ib3_9 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.11", javaVersion = "8")
-val ib3_10 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.12", javaVersion = "8")
-val ib3_11 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.13", javaVersion = "8")
-val ib3_12 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.14", javaVersion = "8")
-val ib3_13 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.15", javaVersion = "8")
-val ib3_14 = IndividualBuildType(type = "Full", os = "Linux", eclipseVersion = "4.16", javaVersion = "8")
-val ib3_15 = IndividualBuildType(type = "Full", os = "Windows", eclipseVersion = "4.4", javaVersion = "8")
-val ib3_16 = IndividualBuildType(type = "Full", os = "Windows", eclipseVersion = "4.7", javaVersion = "8")
-val ib4_1 = IndividualBuildType(type = "CrossVersion", os = "Linux", eclipseVersion = "4.7", javaVersion = "9")
+val ib1_1 = IndividualScenarioBuildType(type = "SanityCheck", os = "Linux", eclipseVersion = "4.13", javaVersion = "8")
+val ib2_1 = IndividualScenarioBuildType(type = "Basic", os = "Linux", eclipseVersion = "4.3", javaVersion = "8")
+val ib2_2 = IndividualScenarioBuildType(type = "Basic", os = "Linux", eclipseVersion = "4.12", javaVersion = "8")
+val ib2_3 = IndividualScenarioBuildType(type = "Basic", os = "Windows", eclipseVersion = "4.3", javaVersion = "8")
+val ib2_4 = IndividualScenarioBuildType(type = "Basic", os = "Windows", eclipseVersion = "4.6", javaVersion = "8")
+val ib3_1 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.4", javaVersion = "8")
+val ib3_2 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.5", javaVersion = "8")
+val ib3_3 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.6", javaVersion = "8")
+val ib3_4 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.7", javaVersion = "8")
+val ib3_5 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.7", javaVersion = "9")
+val ib3_6 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.8", javaVersion = "8")
+val ib3_7 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.9", javaVersion = "8")
+val ib3_8 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.10", javaVersion = "8")
+val ib3_9 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.11", javaVersion = "8")
+val ib3_10 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.12", javaVersion = "8")
+val ib3_11 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.13", javaVersion = "8")
+val ib3_12 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.14", javaVersion = "8")
+val ib3_13 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.15", javaVersion = "8")
+val ib3_14 = IndividualScenarioBuildType(type = "Full", os = "Linux", eclipseVersion = "4.16", javaVersion = "8")
+val ib3_15 = IndividualScenarioBuildType(type = "Full", os = "Windows", eclipseVersion = "4.4", javaVersion = "8")
+val ib3_16 = IndividualScenarioBuildType(type = "Full", os = "Windows", eclipseVersion = "4.7", javaVersion = "8")
+val ib4_1 = IndividualScenarioBuildType(type = "CrossVersion", os = "Linux", eclipseVersion = "4.7", javaVersion = "9")
 
-val individualBuildsForStage1 = listOf(ib1_1)
-val individualBuildsForStage2 = listOf(ib2_1, ib2_2, ib2_3, ib2_4)
-val individualBuildsForStage3 = listOf(ib3_1, ib3_2, ib3_3, ib3_4, ib3_5, ib3_6, ib3_7, ib3_8, ib3_9, ib3_10, ib3_11,  ib3_12, ib3_13, ib3_14, ib3_15, ib3_16)
-val individualBuildsForStage4 = listOf(ib4_1)
+val individualBuildsForPhase1 = listOf(ib1_1)
+val individualBuildsForPhase2 = listOf(ib2_1, ib2_2, ib2_3, ib2_4)
+val individualBuildsForPhase3 = listOf(ib3_1, ib3_2, ib3_3, ib3_4, ib3_5, ib3_6, ib3_7, ib3_8, ib3_9, ib3_10, ib3_11,  ib3_12, ib3_13, ib3_14, ib3_15, ib3_16)
+val individualBuildsForPhase4 = listOf(ib4_1)
 
-val sb1 = StageBuildType("Sanity Check", individualBuildsForStage1)
-val sb2 = StageBuildType("Basic Coverage", individualBuildsForStage2)
-val sb3 = StageBuildType("Full Coverage", individualBuildsForStage3)
-val sb4 = StageBuildType("CrossVersion Coverage", individualBuildsForStage4)
+val tb1_1 = TriggerBuildType("Sanity Check (Trigger)", individualBuildsForPhase1, null)
+val tb1_2 = TriggerBuildType("Basic Test Coverage (Trigger, Phase 1/2)", individualBuildsForPhase1, null)
+val tb2_2 = TriggerBuildType("Basic Test Coverage (Phase 2/2)", individualBuildsForPhase2, tb1_2)
+val tb3_1 = TriggerBuildType("Full Test Coverage (Trigger, Phase 1/3)", individualBuildsForPhase1, null)
+val tb3_2 = TriggerBuildType("Full Test Coverage (Phase 2/3)", individualBuildsForPhase2, tb3_1)
+val tb3_3 = TriggerBuildType("Full Test Coverage (Phase 3/3)", individualBuildsForPhase3, tb3_2)
+val tb4_1 = TriggerBuildType("Cross-Version Coverage (Trigger, Phase 1/4)", individualBuildsForPhase1, null)
+val tb4_2 = TriggerBuildType("Cross-Version Test Coverage (Phase 2/4)", individualBuildsForPhase2, tb4_1)
+val tb4_3 = TriggerBuildType("Cross-Version Test Coverage (Phase 3/4)", individualBuildsForPhase3, tb4_2)
+val tb4_4 = TriggerBuildType("Cross-Version Test Coverage (Phase 4/4)", individualBuildsForPhase4, tb4_3)
 
-val tb1_1 = TriggerBuildType("Sanity Check (Trigger)", sb1, null)
-val tb1_2 = TriggerBuildType("Basic Test Coverage (Trigger, Phase 1/2)", sb1, null)
-val tb2_2 = TriggerBuildType("Basic Test Coverage (Phase 2/2)", sb2, tb1_2)
-val tb3_1 = TriggerBuildType("Full Test Coverage (Trigger, Phase 1/3)", sb1, null)
-val tb3_2 = TriggerBuildType("Full Test Coverage (Phase 2/3)", sb2, tb3_1)
-val tb3_3 = TriggerBuildType("Full Test Coverage (Phase 3/3)", sb2, tb3_2)
-val tb4_1 = TriggerBuildType("Cross-Version Coverage (Trigger, Phase 1/4)", sb1, null)
-val tb4_2 = TriggerBuildType("Cross-Version Test Coverage (Phase 2/4)", sb2, tb4_1)
-val tb4_3 = TriggerBuildType("Cross-Version Test Coverage (Phase 3/4)", sb3, tb4_2)
-val tb4_4 = TriggerBuildType("Cross-Version Test Coverage (Phase 4/4)", sb4, tb4_3)
-
-class BuildshipBuilds : Project({
+object BuildshipBuilds : Project({
     name = "Individual coverage scenarios"
     id("Buildship_individual_coverage_scenarios")
     // TODO both EclipseBuildTemplate and the other template should be here
-    buildTypesWithOrder(individualBuildsForStage1 + individualBuildsForStage2 + individualBuildsForStage3 + individualBuildsForStage4)
+    buildTypesWithOrder(individualBuildsForPhase1 + individualBuildsForPhase2 + individualBuildsForPhase3 + individualBuildsForPhase4)
 })
 
-class BuildshipStages : Project({
-    name = "Stages"
-    id("Buildship_stages")
-    buildTypesWithOrder(listOf(sb1, sb2, sb3, sb4))
-})
-
-class BuildshipTriggers : Project({
+object BuildshipTriggers : Project({
     name = "Triggers"
     id("Buildship_triggers")
     buildTypesWithOrder(listOf(
@@ -105,7 +93,7 @@ class BuildshipTriggers : Project({
         tb4_1, tb4_2, tb4_3, tb4_4))
 })
 
-class IndividualBuildType(type: String, os: String, eclipseVersion: String, javaVersion: String) : BuildType({
+class IndividualScenarioBuildType(type: String, os: String, eclipseVersion: String, javaVersion: String) : BuildType({
     createId("Individual", "${type}_Test_Coverage_${os}_Eclipse${eclipseVersion.replace(".", "_")}_Java${javaVersion}")
 
     templates(EclipseBuildTemplate)
@@ -131,15 +119,7 @@ class IndividualBuildType(type: String, os: String, eclipseVersion: String, java
     }
 })
 
-class StageBuildType(stageName: String, deps: List<BuildType>) : BuildType({
-    createId("Stage", stageName)
-
-    dependencies {
-        deps.forEach { d -> snapshot(d, DefaultFailureCondition) }
-    }
-})
-
-class TriggerBuildType(triggerName: String, stageDependency: StageBuildType, prev: TriggerBuildType?) : BuildType({
+class TriggerBuildType(triggerName: String, scenarios: List<IndividualScenarioBuildType>, prev: TriggerBuildType?) : BuildType({
     createId("Trigger", triggerName)
 
     if (prev != null) {
@@ -156,8 +136,11 @@ class TriggerBuildType(triggerName: String, stageDependency: StageBuildType, pre
         }
     }
 
+
     dependencies {
-        snapshot(stageDependency, DefaultFailureCondition)
+        scenarios.forEach {
+            snapshot(it, DefaultFailureCondition)
+        }
     }
 })
 
@@ -171,7 +154,6 @@ private fun Project.buildTypesWithOrder(buildTypes: List<BuildType>) {
 private fun BuildType.createId(type: String, name: String)  {
     this.name = name
     id("${type}_${name.replace(" ", "_").replace("-", "_").replace("(", "").replace(")", "").replace("/", "").replace(",", "")}")
-
 }
 
 private val DefaultFailureCondition : SnapshotDependency.() -> Unit
