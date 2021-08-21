dfdc09e413e6fdac795681fa8188adacfceab4ea
Donat Csikos <csdonat@gmail.com>
2021-08-21 17:48:30 +0200

    Update TC DSL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -29,5 +29,5 @@ node (Plugins -> teamcity-configs -> teamcity-configs:generate),
 the 'Debug' option is available in the context menu for the task.
 */
 
-version = "2020.2"
+version = "2021.1"
 project(Buildship.Project)
