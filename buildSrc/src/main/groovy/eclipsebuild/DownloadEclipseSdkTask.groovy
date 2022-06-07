b17e086e5db5356fa993250171192185cca3a387
Donát Csikós <csdonat@gmail.com>
2022-06-07 06:18:32 -0700

    Save eclipse sdk to local build directory

--- buildSrc/src/main/groovy/eclipsebuild/DownloadEclipseSdkTask.groovy
+++ buildSrc/src/main/groovy/eclipsebuild/DownloadEclipseSdkTask.groovy
@@ -1,43 +1,46 @@
 package eclipsebuild
 
-
 import com.google.common.hash.Hashing
 import com.google.common.hash.HashingOutputStream
 import org.apache.commons.io.FileUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.os.OperatingSystem
 
+import javax.inject.Inject
 import java.nio.file.Files
 
 @CacheableTask
-class DownloadEclipseSdkTask extends DefaultTask {
+abstract class DownloadEclipseSdkTask extends DefaultTask {
 
     @Input
-    String downloadUrl
+    abstract Property<String> getDownloadUrl()
 
     @Input
-    String expectedSha256Sum
+    abstract Property<String> getExpectedSha256Sum()
 
     @OutputDirectory
-    File targetDir
+    abstract DirectoryProperty getTargetDir()
 
-    DownloadEclipseSdkTask() {
-        onlyIf {
-            File digestFile = new File(targetDir, 'digest')
-            !digestFile.exists() || digestFile.text != expectedSha256Sum
-        }
+    private final Config config
+
+    @Inject
+    DownloadEclipseSdkTask(Config config) {
+        this.config = config
+        targetDir.set(config.eclipseSdkDir)
     }
 
     @TaskAction
-    public void downloadSdk() {
+    void downloadSdk() {
         // if multiple builds start on the same machine (which is the case with a CI server)
         // we want to prevent them downloading the same file to the same destination
-        def directoryLock = new FileSemaphore(targetDir)
+        def directoryLock = new FileSemaphore(targetDir.get().asFile)
         directoryLock.lock()
         try {
             downloadEclipseSdkUnprotected(getProject())
@@ -48,28 +51,28 @@ class DownloadEclipseSdkTask extends DefaultTask {
 
     private void downloadEclipseSdkUnprotected(Project project) {
         // download the archive
-        File destination = new File(targetDir, 'eclipse-sdk.archive')
-        project.logger.info("Download Eclipse SDK from '${downloadUrl}' to '$destination.absolutePath'")
-        project.ant.get(src: new URL(downloadUrl), dest: destination)
-        verifyAndSaveSdk256Sum(targetDir, destination)
+        File destination = new File(targetDir.get().asFile, 'eclipse-sdk.archive')
+        project.logger.info("Download Eclipse SDK from '${downloadUrl.get()}' to '$destination.absolutePath'")
+        project.ant.get(src: new URL(downloadUrl.get()), dest: destination)
+        verifySdk256Sum(destination)
 
         // extract it to the same location where it was extracted
-        project.logger.info("Extract 'archive to '$targetDir.absolutePath'")
+        project.logger.info("Extract 'archive to '${targetDir.get().asFile.absolutePath}'")
         if (OperatingSystem.current().isWindows()) {
-            project.ant.unzip(src: destination, dest: targetDir, overwrite: true)
+            project.ant.unzip(src: destination, dest: targetDir.get().asFile, overwrite: true)
         } else if (OperatingSystem.current().isMacOsX()) {
             try {
                 project.exec { commandLine 'hdiutil', 'attach', destination.absolutePath }
-                FileUtils.copyDirectory(new File('/Volumes/Eclipse/Eclipse.app'), new File(targetDir, 'Eclipse.app'))
+                FileUtils.copyDirectory(new File('/Volumes/Eclipse/Eclipse.app'), new File(targetDir.get().asFile, 'Eclipse.app'))
             } finally {
                 project.exec { commandLine 'hdiutil', 'detach', '/Volumes/Eclipse' }
             }
         } else {
-            project.ant.untar(src: destination, dest: targetDir, compression: "gzip", overwrite: true)
+            project.ant.untar(src: destination, dest: targetDir.get().asFile, compression: "gzip", overwrite: true)
         }
 
         // make it executable
-        File exe = eclipseSdkExe()
+        File exe = config.eclipseSdkExe
         project.logger.info("Set '${exe}' executable")
         exe.setExecutable(true)
 
@@ -79,16 +82,11 @@ class DownloadEclipseSdkTask extends DefaultTask {
         }
     }
 
-    private void verifyAndSaveSdk256Sum(File targetDir,  File destination) {
+    private void verifySdk256Sum(File destination) {
         def actualSha256Sum = calculateSdk256Sum(destination)
-        if (actualSha256Sum != expectedSha256Sum) {
-            throw new RuntimeException("Eclipse SDK SHA-126 sum does not match. Expected: $expectedSha256Sum, actual: $actualSha256Sum.")
+        if (actualSha256Sum != expectedSha256Sum.get()) {
+            throw new RuntimeException("Eclipse SDK SHA-126 sum does not match. Expected: $expectedSha256Sum.get(), actual: $actualSha256Sum.")
         }
-        new File(targetDir, 'digest').text = actualSha256Sum
-    }
-
-    private File eclipseSdkExe() {
-        new File(targetDir, Constants.eclipseExePath)
     }
 
     private static String calculateSdk256Sum(File destination) {
