831dd6159965d612fbd439f1bfc54ace257e8918
Donat Csikos <csdonat@gmail.com>
2022-07-12 10:49:20 +0200

    Update TC VcsRoot

--- .teamcity/Buildship/GitHubVcsRoot.kt
+++ .teamcity/Buildship/GitHubVcsRoot.kt
@@ -5,9 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 object GitHubVcsRoot : GitVcsRoot({
     name = "Buildship"
     url = "https://github.com/eclipse/buildship.git"
-    branchSpec = "+:refs/heads/*"
     agentGitPath = "%env.TEAMCITY_GIT_PATH%"
     agentCleanFilesPolicy = AgentCleanFilesPolicy.NON_IGNORED_ONLY
-    useMirrors = false
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
     authMethod = anonymous()
 })
