b4958c7a6c32c3377b29818662322888e2822785
kolavladimirov <be2carefull@yandex.ru>
2023-06-13 17:18:10 +0200

    plugin is configured to run language server

--- org.eclipse.buildship.kts.scripts/com.kotlin.languageserver/src/com/kotlin/languageserver/GradlePropertiesConnectionProvider.java
+++ org.eclipse.buildship.kts.scripts/com.kotlin.languageserver/src/com/kotlin/languageserver/GradlePropertiesConnectionProvider.java
@@ -1,21 +1,23 @@
 package com.kotlin.languageserver;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
 
 public class GradlePropertiesConnectionProvider extends ProcessStreamConnectionProvider implements StreamConnectionProvider {
 	public GradlePropertiesConnectionProvider() {
-		System.out.println("starting server...");
+		System.out.println("starting...");
 		
 		List<String> commands = new ArrayList<>();
-		
+		commands.add("bash");
+		commands.add("/Users/kolavladimirov/launchServer.sh");
+
 		// add in commands path to bin application of language server
-		
 		setCommands(commands);
-		
 		setWorkingDirectory(System.getProperty("user.dir"));
 	}
 	
