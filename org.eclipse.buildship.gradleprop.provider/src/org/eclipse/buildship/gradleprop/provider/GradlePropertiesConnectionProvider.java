53705cab5d7eefe1e605acc8caff77aad3cdb7ab
kolavladimirov <be2carefull@yandex.ru>
2023-06-26 13:13:47 +0200

    provider is rewrited to launch server without scripts

--- org.eclipse.buildship.gradleprop.provider/src/org/eclipse/buildship/gradleprop/provider/GradlePropertiesConnectionProvider.java
+++ org.eclipse.buildship.gradleprop.provider/src/org/eclipse/buildship/gradleprop/provider/GradlePropertiesConnectionProvider.java
@@ -1,38 +1,66 @@
 package org.eclipse.buildship.gradleprop.provider;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
-//import org.osgi.framework.FrameworkUtil;
+import org.eclipse.core.runtime.FileLocator;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.Bundle;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class GradlePropertiesConnectionProvider extends ProcessStreamConnectionProvider
 		implements StreamConnectionProvider {
 	public GradlePropertiesConnectionProvider() {
 		System.out.println("starting...");
+		
+		
+		URL localFileURL;
+		String launch = "";
+		Bundle bundle = FrameworkUtil.getBundle(GradlePropertiesConnectionProvider.class);
 		try {
-			Process p = new ProcessBuilder("bash", "/Users/kolavladimirov/initialize.sh").start();
-			int exitCode = p.waitFor();
-			if (exitCode != 0) {
-				System.exit(exitCode);
-			}
+			
+			localFileURL = FileLocator.toFileURL(bundle.getEntry("/"));
+			URI localFileURI= new URI(localFileURL.toExternalForm());
+			
+			Path p = Paths.get(localFileURI.getPath());
+
+			launch = p.resolve("jars/server.jar").toString();
+			System.err.println("init pathes");
+			
+//			System.err.println(new File(jar.toString()).exists());
+//			Process pr = new ProcessBuilder("bash", init.toString()).start();
+//			int exitCode = pr.waitFor();
+//			if (exitCode != 0) {
+//				System.err.println("exit code is " + exitCode);
+//				System.exit(exitCode);
+//			}
 			System.out.println("initializing of server is success");
-		} catch (IOException | InterruptedException e) {
+	        
+	        
+	        List<String> commands = new ArrayList<>();
+			commands.add("java");
+			commands.add("-jar");
+			commands.add(launch);
+			// add in commands path to bin application of language server
+			setCommands(commands);
+
+			setWorkingDirectory(p.toString());
+	        
+		} catch (IOException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			System.out.println("error is catched:" + e.toString());
 			e.printStackTrace();
-			System.exit(1);
 		}
 
-
-		List<String> commands = new ArrayList<>();
-		commands.add("bash");
-		commands.add("/Users/kolavladimirov/launchServer.sh");
-		// add in commands path to bin application of language server
-		setCommands(commands);
-
-		setWorkingDirectory(System.getProperty("user.dir"));
 	}
 
-	// new version
 }
