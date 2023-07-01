0e4968d1ed469be80b80d1f7ec75d542cebccdf6
kolavladimirov <be2carefull@yandex.ru>
2023-07-01 11:52:09 +0300

    scripts plugin has been removed

--- org.eclipse.buildship.gradleprop.scripts/initialize.sh
+++
@@ -1,4 +0,0 @@
-cd #path to language-server-for-gradle-properties
-
-# pack server to jar
-./gradlew shadowJar
--- org.eclipse.buildship.gradleprop.scripts/launchServer.sh
+++
@@ -1,3 +0,0 @@
-cd #path to language-server-for-gradle-properties
-
-java -jar jars/server.jar
