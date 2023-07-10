a60c48f0599d0201e8adfb9cef027004d4371115
kolavladimirov <be2carefull@yandex.ru>
2023-07-10 14:59:35 +0300

    removed settings.gradle.kts from language server implementation

--- org.eclipse.buildship.gradleprop.ls/settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
-}
-
-rootProject.name = "server"
