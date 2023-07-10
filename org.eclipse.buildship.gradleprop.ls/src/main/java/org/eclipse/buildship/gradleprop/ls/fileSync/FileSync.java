3484c6cc7972d7efaa23a2dd087fd805e2bf9d3a
kolavladimirov <be2carefull@yandex.ru>
2023-07-10 14:57:17 +0300

    removed reduntant import to pass checkStyle

--- org.eclipse.buildship.gradleprop.ls/src/main/java/org/eclipse/buildship/gradleprop/ls/fileSync/FileSync.java
+++ org.eclipse.buildship.gradleprop.ls/src/main/java/org/eclipse/buildship/gradleprop/ls/fileSync/FileSync.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Map;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextDocumentContentChangeEvent;
-import org.eclipse.buildship.gradleprop.ls.fileSync.ContentInFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
