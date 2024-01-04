2887f26ac459483b500a81b6ed82c63bb45f7269
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-01-04 16:35:01 +0100

    Remove TeamCity configuration (#1290)

--- .teamcity/Buildship/ScenarioType.kt
+++
@@ -1,8 +0,0 @@
-package Buildship
-
-enum class ScenarioType(val gradleTasks: String) {
-    SANITY_CHECK("assemble checkstyleMain"),
-    BASIC_COVERAGE("clean eclipseTest"),
-    FULL_COVERAGE("clean build"),
-    CROSS_VERSION("clean crossVersionEclipseTest")
-}
